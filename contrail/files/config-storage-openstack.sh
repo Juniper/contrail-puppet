ebff075fcbe983174a4dc8faf91fcccb43551539
Dheeraj Gautam <dgautam@juniper.net>
2014-08-05 14:12:11 -0700

    added rabbit_host to cinder.conf

--- contrail/files/config-storage-openstack.sh
+++ contrail/files/config-storage-openstack.sh
@@ -1,7 +1,10 @@
+set -x
 virsh_secret=$1
+openstack_ip=$2
 sed -i "s/^bind-address/#bind-address/" /etc/mysql/my.cnf
 openstack-config --set /etc/cinder/cinder.conf DEFAULT sql_connection mysql://cinder:cinder@127.0.0.1/cinder
 openstack-config --set /etc/cinder/cinder.conf DEFAULT enabled_backends rbd-disk
+openstack-config --set /etc/cinder/cinder.conf DEFAULT rabbit_host  ${openstack_ip}
 openstack-config --set /etc/cinder/cinder.conf rbd-disk rbd_pool volumes
 openstack-config --set /etc/cinder/cinder.conf rbd-disk rbd_user volumes
 openstack-config --set /etc/cinder/cinder.conf rbd-disk rbd_secret_uuid $virsh_secret
@@ -16,9 +19,10 @@ openstack-config --set /etc/glance/glance-api.conf DEFAULT rbd_store_user images
 cinder type-create ocs-block-disk
 cinder type-key ocs-block-disk set volume_backend_name=RBD
 
-avail=$(rados df | grep avail | awk  '{ print $3 }')
-let "avail_gb = ${avail} / (1024*1024)"
-cinder quota-update ocs-block-disk --gigabytes ${avail_gb}
+#avail=$(rados df | grep avail | awk  '{ print $3 }')
+## 1024*1024 => 1048576
+#avail_gb=$(expr ${avail} / 1048576)
+#cinder quota-update ocs-block-disk --gigabytes ${avail_gb}
 
 cinder-manage db sync
 
