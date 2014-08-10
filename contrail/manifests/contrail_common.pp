6151f4f7578e066c2979c87ea9bf300ecc6a1d8a
tsurendra <tsurendra@juniper.net>
2014-08-10 00:41:18 -0700

    [Status checkin]

--- contrail/manifests/contrail_common.pp
+++ contrail/manifests/contrail_common.pp
@@ -202,6 +202,25 @@ define contrail-install-repo(
     }
 }
 
+define report_status($state) {
+	if ! defined(Package['curl']) {
+		package { 'curl' : ensure => present,}
+
+
+
+	}
+    exec { "contrail-status-$state" :
+        command => "mkdir -p /etc/contrail/ && curl -X PUT \"http://$serverip:9002/server_status?server_id=$hostname&state=$state\" && echo contrail-status-$state >> /etc/contrail/contrail_common_exec.out",
+        provider => shell,
+	require => Package["curl"],
+        unless  => "grep -qx contrail-status-$state /etc/contrail/contrail_common_exec.out",
+        logoutput => "true"
+    }
+
+
+}
+
+
 # macro to perform common functions
 # Following variables need to be set for this resource.
 #     $self_ip
