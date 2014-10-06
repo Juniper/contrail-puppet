001a66cd130eab96aaff9ec3e6d07ec44c287640
Dheeraj Gautam <dgautam@juniper.net>
2014-10-06 00:23:15 -0700

    re-commit
    Added two more fixes:
    1. added check for number of OSD configured v/s actually configured
    2. added dependency for configuring openstack before configuring
    	live migration
    
    Fixes for supporting NFS-Live-Migration
    1. added script for openstack node.
    2. script for compute node
    3. added openstack-get-config to query config of a parameter
    
    Change-Id: I4f819993f6c18beb5a180c040c99573ce771af79

---
+++ contrail/files/config-storage-lm-compute.sh
@@ -0,0 +1,134 @@
+#!/bin/sh
+
+set -x
+RETVAL=0
+VM_HOST=0
+VM_HOSTNAME=$1
+MY_HOSTNAME=`hostname`
+
+if [ ${VM_HOSTNAME} != ${MY_HOSTNAME} ]
+then
+  VM_HOST=0
+else
+  VM_HOST=1
+fi
+
+echo "I am VM_HOST = ${VM_HOST}"
+
+cat /proc/mounts  | grep -q /var/lib/nova/instances/global
+RETVAL=$?
+if [ $RETVAL -eq 0 ]
+then 
+  echo "VM is mounted, not doing anything"
+  exit 0
+fi
+
+
+CONFIG_VALUE=`/etc/contrail/contrail_setup_utils/openstack-get-config --get /etc/nova/nova.conf DEFAULT live_migration_flag`
+RETVAL=$?
+echo $?
+echo ${CONFIG_VALUE}
+if [ $RETVAL -ne 0 ]
+then 
+  echo "openstack-get-config failed, configure values "
+  openstack-config --set /etc/nova/nova.conf DEFAULT live_migration_flag VIR_MIGRATE_UNDEFINE_SOURCE,VIR_MIGRATE_PEER2PEER,VIR_MIGRATE_LIVE
+  openstack-config --set /etc/nova/nova.conf DEFAULT vncserver_listen 0.0.0.0
+  cat /etc/libvirt/libvirtd.conf | sed s/"#listen_tls = 0"/"listen_tls = 0"/ | sed s/"#listen_tcp = 1"/"listen_tcp = 1"/ | sed s/'#auth_tcp = "sasl"'/'auth_tcp = "none"'/ > /tmp/libvirtd.conf
+  cp -f  /tmp/libvirtd.conf  /etc/libvirt/libvirtd.conf
+  
+  cat /etc/default/libvirt-bin | sed s/"-d"/"-d -l"/ > /tmp/libvirtd.tmp
+  cp -f /tmp/libvirtd.tmp /etc/default/libvirt-bin
+  service nova-compute restart
+  service libvirt-bin restart
+  CONFIG_VALUE=`/etc/contrail/contrail_setup_utils/openstack-get-config --get /etc/nova/nova.conf DEFAULT live_migration_flag`
+fi
+
+## check config values again for comparision
+if [ ! "x${CONFIG_VALUE}" = "xVIR_MIGRATE_UNDEFINE_SOURCE,VIR_MIGRATE_PEER2PEER,VIR_MIGRATE_LIVE" ]
+then
+  echo "live_migration_flag value is not correct"
+  openstack-config --set /etc/nova/nova.conf DEFAULT live_migration_flag VIR_MIGRATE_UNDEFINE_SOURCE,VIR_MIGRATE_PEER2PEER,VIR_MIGRATE_LIVE
+  openstack-config --set /etc/nova/nova.conf DEFAULT vncserver_listen 0.0.0.0
+  if [ ${VM_HOST} -eq 1 ]
+  then
+    openstack-config --set /etc/nova/nova.conf DEFAULT resume_guests_state_on_host_boot True
+  fi
+  cat /etc/libvirt/libvirtd.conf | sed s/"#listen_tls = 0"/"listen_tls = 0"/ | sed s/"#listen_tcp = 1"/"listen_tcp = 1"/ | sed s/'#auth_tcp = "sasl"'/'auth_tcp = "none"'/ > /tmp/libvirtd.conf
+  cp -f  /tmp/libvirtd.conf  /etc/libvirt/libvirtd.conf
+  
+  cat /etc/default/libvirt-bin | sed s/"-d"/"-d -l"/ > /tmp/libvirtd.tmp
+  cp -f /tmp/libvirtd.tmp /etc/default/libvirt-bin
+  service nova-compute restart
+  service libvirt-bin restart
+fi
+
+
+mkdir -p /var/lib/nova/instances/global
+chown nova:nova /var/lib/nova/instances/global
+
+
+if [ ${VM_HOST} -eq 1 ]
+then
+  ifconfig livemnfsvgw
+  RETVAL=$?
+  if [ ${RETVAL} -ne 0 ]
+  then
+    vif --create livemnfsvgw --mac 00:01:5e:00:00
+    ifconfig livemnfsvgw up
+  fi
+  grep -q "pre-up vif --create livemnfsvgw --mac 00:01:5e:00:00" /etc/network/interfaces
+  RETVAL=$?
+  if [ ${RETVAL} -ne 0 ]
+  then
+    echo \"\" >> /etc/network/interfaces
+    echo \"auto livemnfsvgw\" >> /etc/network/interfaces
+    echo \"iface livemnfsvgw inet manual\" >> /etc/network/interfaces
+    echo \"    pre-up vif --create livemnfsvgw --mac 00:01:5e:00:00\" >> /etc/network/interfaces
+    echo \"    pre-up ifconfig livemnfsvgw up\" >> /etc/network/interfaces
+  fi
+
+  CONFIG_VALUE=`/etc/contrail/contrail_setup_utils/openstack-get-config --get /etc/contrail/contrail-vrouter-agent.conf GATEWAY-1 ip_blocks`
+  RETVAL=$?
+  if [ ${RETVAL} -ne 0 ] || [ ! "x${CONFIG_VALUE}" = "x192.168.101.0/24" ]
+  then
+    openstack-config --set /etc/contrail/contrail-vrouter-agent.conf GATEWAY-1 routing_instance default-domain:admin:livemnfs:livemnfs
+    openstack-config --set /etc/contrail/contrail-vrouter-agent.conf GATEWAY-1 interface livemnfsvgw
+    openstack-config --set /etc/contrail/contrail-vrouter-agent.conf GATEWAY-1 ip_blocks 192.168.101.0/24
+    service contrail-vrouter-agent restart
+  fi
+  
+  netstat -nr | grep -q 192.168.101.2
+  if [ ${RETVAL} -ne 0 ]
+  then
+    route add -host 192.168.101.2/32 dev livemnfsvgw
+  fi
+  grep -q "up route add -host 192.168.101.2/32 dev livemnfsvgw" /etc/network/interfaces
+  RETVAL=$?
+  if [ ${RETVAL} -ne 0 ]
+  then
+    echo "up route add -host 192.168.101.2/32 dev livemnfsvgw" >> /etc/network/interfaces
+  fi
+else 
+  netstat -nr | grep -q 192.168.101.2
+  RETVAL=$?
+  if [ ${RETVAL} -ne 0 ]
+  then
+    route add 192.168.101.2 dev vhost0
+  fi
+
+  grep -q "up route add 192.168.101.2 dev vhost0" /etc/network/interfaces
+  RETVAL=$?
+  if [ ${RETVAL} -ne 0 ]
+  then
+    echo "up route add 192.168.101.2 dev vhost0" >> /etc/network/interfaces
+  fi
+fi
+
+
+ping -c 5 192.168.101.2
+RETVAL=$?
+if [ $RETVAL -eq 0 ]
+then 
+  echo "ping done"
+  mount 192.168.101.2:/livemnfsvol /var/lib/nova/instances/global
+fi
