362c7d72f4e91b5c46a97e253a2df84527a47b6f
Dheeraj Gautam <dgautam@juniper.net>
2015-01-29 05:59:28 -0800

    sm-storage: fixed base installation of storage via puppet
    copied old working file from puppet repo. Current files were not last
    known working files.
    
    unit testing:
    1. Installation on multiple setup.
    2. Creation of volume, attaching it to a instance.
    
    Change-Id: I454d39bd4d5098c0ad510d6b0296122e74ab385b
    Partial-Bug: #1415435

--- contrail/environment/modules/contrail/files/config-storage-disk-clean.sh
+++ contrail/environment/modules/contrail/files/config-storage-disk-clean.sh
@@ -3,12 +3,16 @@ set -x
 
 disk_names=$1
 
+## Segregation of journal disk and data disk
 osd_disk=`echo ${disk_names} | awk -F ':' '{printf $1}'`
 journal_disk=`echo ${disk_names} | awk -F ':' '{printf $2}'`
 
 echo "OSD disk : ${osd_disk}"
 echo "Journal disk : ${journal_disk}"
 
+## Check for valid inputs.
+## NOTE: Journal disk could be empty, same disk is to be used for journal
+## NOTE: as well
 if [ -z ${osd_disk}  ]
 then
   echo "Disk name empty, Invalid Input"
@@ -17,6 +21,7 @@ fi
 
 check_disk_avail ()
 {
+  ## Check if disk exists
   disk_name=$1
   /sbin/fdisk -l ${disk_name}
   RETVAL=$?
@@ -26,6 +31,7 @@ check_disk_avail ()
     exit ${RETVAL}
   fi
 
+  ## Check if disk is a part of LVM
   pvdisplay | grep -q ${disk_name}
   RETVAL=$?
   if [ ${RETVAL} -eq 0 ] 
@@ -39,11 +45,14 @@ check_disk_avail ()
 
 clean_disk_parts()
 {
+  ## Get the disk GUID and check against existing cleaned disks.
+  ## This is to avoid cleaning a disk multiple times.
   disk_name=$1
   disk_guid=`sgdisk -p ${disk_name}  | grep "Disk identifier (GUID):" | awk '{printf $4}'`
 
   if [ -f /etc/contrail/config-storage-clean-disk.out ] 
   then
+    ## Check if disk GUID exists in cleaned disks list
     grep -q ${disk_guid} /etc/contrail/config-storage-clean-disk.out
     RETVAL=$?
     if [ ${RETVAL} -eq 0 ]
@@ -53,6 +62,7 @@ clean_disk_parts()
     fi
   fi
   
+  ## clean partition table
   dd if=/dev/zero of=${disk_name} bs=512 count=1
   RETVAL=$?
   if [ ${RETVAL} -ne 0 ]
@@ -61,6 +71,7 @@ clean_disk_parts()
     exit ${RETVAL}
   fi
   
+  ## convert the disk to GPT
   parted -s ${disk_name} mklabel gpt
   RETVAL=$?
   if [ ${RETVAL} -ne 0 ]
@@ -71,16 +82,19 @@ clean_disk_parts()
   
   new_disk_guid=`sgdisk -p ${disk_name}  | grep "Disk identifier (GUID):" | awk '{printf $4}'`
 
+  ## put disk GUID to clean-up data-base 
   echo "${new_disk_guid}" >> /etc/contrail/config-storage-clean-disk.out
 }
 
+## CHeck the  data disks
 check_disk_avail ${osd_disk}
 clean_disk_parts ${osd_disk}
 
+## clean journal disk as well.
 if [ x${journal_disk} != x"" ]
 then
   echo "cleaning journal disks"
+  ## Check journal disks as well
   check_disk_avail ${journal_disk}
-
   clean_disk_parts ${journal_disk}
 fi
