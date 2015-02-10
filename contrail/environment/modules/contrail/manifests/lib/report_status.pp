52090dd4cc0c934350eb225204f6bc62b029a6c4
Prasad Miriyala <pmiriyala@juniper.net>
2015-02-10 15:27:07 -0800

    This fix goes in conjection with sm commit to change the environment mapping after provision completed. sm commit id 18bfc1d0ef5ce74e6d68512700cdf9e2fc6916ce This fix sends status to the server manager even if it is reprovisioning, and server manager will stop the agent after it receives the provision complete.
    Change-Id: I7303360eab805c58e72cc4bcbba596534d2fd70f

--- contrail/environment/modules/contrail/manifests/lib/report_status.pp
+++ contrail/environment/modules/contrail/manifests/lib/report_status.pp
@@ -4,7 +4,6 @@ define contrail::lib::report_status(
     exec { "contrail-status-$state" :
 	command => "mkdir -p /etc/contrail/ && wget --post-data=\"\" \"http://$serverip:9002/server_status?server_id=$hostname&state=$state\" && echo contrail-status-$state >> /etc/contrail/contrail_common_exec.out",
 	provider => shell,
-	unless  => "grep -qx contrail-status-$state /etc/contrail/contrail_common_exec.out",
 	logoutput => "true"
     }
 }
