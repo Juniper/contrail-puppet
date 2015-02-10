bdb7157d01734e43eb3a5617ad912d9584535861
Abhay Joshi <abhayj@juniper.net>
2015-02-10 15:15:33 -0800

    Closes-Bug: #1415103, #1412776     Create openstackrc, keystonerc and ec2rc files for contrail.
    Change-Id: If5e7ee38df008a7c8f6c3b5420b0b1c4d6e5881c

---
+++ contrail/environment/modules/contrail/manifests/contrail_openstack.pp
@@ -0,0 +1,80 @@
+# == Class: contrail::contrail_openstack
+#
+# This class is used to configure software and services required
+# to perfrom any additional functionality required on openstack node
+# by contrail modules (e.g. create openstackrc, keystonerc, ec2rc files etc).
+# Any new code needed to be executed on openstack node by contrail should be
+# added here.
+#
+# === Parameters:
+#
+# [*openstack_ip*]
+#     IP address of server running openstack services. If the server has
+#     separate interfaces for management and control, this parameter
+#     should provide control interface IP address.
+#
+# [*keystone_ip*]
+#     IP address of server running keystone service. Should be specified if
+#     keystone is running on a server other than openstack server.
+#     (optional) - Defaults to "", meaning use openstack_ip.
+#
+# [*internal_vip*]
+#     Virtual mgmt IP address for openstack modules
+#     (optional) - Defaults to ""
+#
+# [*keystone_admin_user*]
+#     Keystone admin user.
+#     (optional) - Defaults to "admin".
+#
+# [*keystone_admin_password*]
+#     Keystone admin password.
+#     (optional) - Defaults to "contrail123"
+#
+# [*keystone_admin_tenant*]
+#     Keystone admin tenant name.
+#     (optional) - Defaults to "admin".
+#
+# [*keystone_service_token*]
+#     openstack service token value.
+#     (optional) - Defaults to "contrail123"
+#
+# [*keystone_auth_protocol*]
+#     Keystone authentication protocol.
+#     (optional) - Defaults to "http".
+#
+class contrail::contrail_openstack (
+    $openstack_ip = $::contrail::params::openstack_ip_list[0],
+    $keystone_ip = $::contrail::params::keystone_ip,
+    $internal_vip = $::contrail::params::internal_vip,
+    $keystone_admin_user = $::contrail::params::keystone_admin_user,
+    $keystone_admin_password = $::contrail::params::keystone_admin_password,
+    $keystone_admin_tenant = $::contrail::params::keystone_admin_tenant,
+    $keystone_service_token = $::contrail::params::keystone_service_token,
+    $keystone_auth_protocol = $::contrail::params::keystone_auth_protocol
+) inherits ::contrail::params {
+    # Main code for class
+    # Create openstackrc file.
+    file { "/etc/contrail/openstackrc" :
+	ensure  => present,
+	content => template("$module_name/openstackrc.erb"),
+    }
+    # Create openstackrc file.
+    file { "/etc/contrail/keystonerc" :
+	ensure  => present,
+	content => template("$module_name/keystonerc.erb"),
+    }
+    # Create ec2rc file
+    file { "/opt/contrail/bin/contrail-create-ec2rc.sh" :
+        ensure  => present,
+        mode => 0755,
+        group => root,
+        source => "puppet:///modules/$module_name/contrail-create-ec2rc.sh"
+    }
+    exec { "exec_create_ec2rc_file":
+        command => "./contrail-create-ec2rc.sh",
+        cwd => "/opt/contrail/bin/",
+        provider => shell,
+        require => [ File["/opt/contrail/bin/contrail-create-ec2rc.sh"] ],
+        logoutput => true
+    }
+}
