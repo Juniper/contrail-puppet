88317591bd086dea46ae57350af84da860e0748c
tsurendra <tsurendra@juniper.net>
2015-02-23 16:12:03 -0800

    Server Manager: provision complete status is lost when compute role is present Closes-Bug: #1424870
    provision status is updated by a curl request to smgr.
    On a compute node, Reboot is issued and then it passes to provision
    completed.
    since reboot is not blocking.
    
    There is a possibility that curl request could be sent or not sent,
    based on when reboot completes.
    
    Fix:
    Move reboot to provision complete stage.
    and in compute just mark for reboot.
    
    Also move the openstack related post operations to a new resource.
    Also fix the report_status to run only once , compute_completed status
    
    Change-Id: Ie1b6db744faf3b33dd4711b491fd9352fe4ad31b

--- contrail/environment/modules/contrail/manifests/provision_complete.pp
+++ contrail/environment/modules/contrail/manifests/provision_complete.pp
@@ -7,32 +7,16 @@ class contrail::provision_complete(
     $internal_vip =  $::contrail::params::internal_vip
 
 
-    contrail::lib::report_status { $state: state => $state }
-    if ($host_control_ip in $openstack_ip_list) {
 
-      package { 'contrail-openstack':
-	ensure  => present,
-      }
-
-
-      exec { "exec_start_supervisor_openstack" :
-	  command => "service supervisor-openstack start && echo start_supervisor_openstack >> /etc/contrail/contrail_openstack_exec.out",
-	  unless  => "grep -qx start_supervisor_openstack /etc/contrail/contrail_openstack_exec.out",
-	  provider => shell,
-	  require => [ Package["contrail-openstack"]  ],
-	  logoutput => 'true'
-      }
-
-      #Make ha-mon start later
-      if($internal_vip != "") {
-            exec { "ha-mon-restart":
-                command => "service contrail-hamon restart && echo contrail-ha-mon >> /etc/contrail/contrail_openstack_exec.out",
-                provider => shell,
-                logoutput => "true",
-                unless  => "grep -qx contrail-ha-mon  /etc/contrail/contrail_openstack_exec.out",
-            }
-      }
-
+    contrail::lib::post_openstack{post_openstack: host_control_ip => $host_control_ip, openstack_ip_list => $openstack_ip_list, internal_vip => $internal_vip}
+    ->
+    contrail::lib::report_status { $state: state => $state}
+    ->
+    exec { "reboot_server" :
+	command => "reboot && reboot_server >> /etc/contrail/contrail_common_exec.out",
+	onlyif  => "grep -qx reboot-server-2 /etc/contrail/contrail_compute_exec.out",
+	provider => shell,
+	logoutput => 'true'
     }
 
 }
