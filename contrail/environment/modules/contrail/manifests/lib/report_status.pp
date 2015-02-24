f41cbfbf6578fe274279cde0b792d8f953702bf1
tsurendra <tsurendra@juniper.net>
2015-02-23 23:31:39 -0800

    Server Manager: provision complete status is lost when compute role is present Closes-Bug: #1424870
    Adding additional check, so that do-reboot-server is done only once.
    Also report status everytime provision is run,
    irrespective of the fact that status is already reported.
    
    Change-Id: I2d84d02d146720faa8968497b2ce61fa06c84257

--- contrail/environment/modules/contrail/manifests/lib/report_status.pp
+++ contrail/environment/modules/contrail/manifests/lib/report_status.pp
@@ -3,7 +3,6 @@ define contrail::lib::report_status(
 ) {
     exec { "contrail-status-$state" :
 	command => "mkdir -p /etc/contrail/ && wget --post-data=\"\" \"http://$serverip:9002/server_status?server_id=$hostname&state=$state\" && echo contrail-status-$state >> /etc/contrail/contrail_common_exec.out",
-        unless => ["grep -qx contrail-status-$state /etc/contrail/contrail_common_exec.out"],
 	provider => shell,
 	logoutput => "true"
     }
