f02b292255742ae21dc478a2676e46d199628d92
tsurendra <tsurendra@juniper.net>
2015-02-27 12:58:30 -0800

    Closes-Bug: #1426235 Server Manger: Upgrade kernel based on ubuntu lsb-release
    Currently server manager tries tp upgrade to 3.13.-34 kernel version for
    both ubuntu 12 and 14.
    Instead , if Ubuntu12- upgrade to 34 and if ubuntu-14 upgrade 40 kernel.
    
    Also, remove templatization of keystone-auth.conf,as it no longer
    needed.
    
    Change-Id: I174f931345008f9f6a70fe402ee43433c5477717

--- contrail/environment/modules/contrail/manifests/lib/upgrade-kernel.pp
+++ contrail/environment/modules/contrail/manifests/lib/upgrade-kernel.pp
@@ -1,16 +1,26 @@
 define contrail::lib::upgrade-kernel($contrail_kernel_upgrade, $contrail_kernel_version) {
-    $headers = "linux-headers-${contrail_kernel_version}"
-    $headers_generic = "linux-headers-${contrail_kernel_version}-generic"
-    $image = "linux-image-${contrail_kernel_version}"
 
     if ($operatingsystem == "Ubuntu" and $contrail_kernel_upgrade == "yes")
     {
         if ($lsbdistrelease == "14.04") {
+            if ($contrail_kernel_version == "" ) {
+                $contrail_dist_kernel_version = "3.13.0-40"
+            } else {
+                $contrail_dist_kernel_version = $contrail_kernel_version
+            }
+
+            $headers = "linux-headers-${contrail_dist_kernel_version}"
+            $headers_generic = "linux-headers-${contrail_dist_kernel_version}-generic"
+            $image = "linux-image-${contrail_dist_kernel_version}-generic"
+            $image_extra = "linux-image-extra-${contrail_dist_kernel_version}-generic"
+
             package { $headers : ensure => present, }
             ->
             package { $headers_generic : ensure => present, }
             ->
             package { $image : ensure => present, }
+            ->
+            package { $image_extra : ensure => present, }
             ->
             exec { "upgrade-kernel-reboot":
                 command => "echo upgrade-kernel-reboot >> /etc/contrail/contrail_common_exec.out && reboot ",
@@ -19,6 +29,17 @@ define contrail::lib::upgrade-kernel($contrail_kernel_upgrade, $contrail_kernel_
                 unless => ["grep -qx upgrade-kernel-reboot /etc/contrail/contrail_common_exec.out"]
             }
         } else {
+            if ($contrail_kernel_version == "" ) {
+                $contrail_dist_kernel_version = "3.13.0-34"
+            } else {
+                $contrail_dist_kernel_version = $contrail_kernel_version
+            }
+
+            $headers = "linux-headers-${contrail_dist_kernel_version}"
+            $headers_generic = "linux-headers-${contrail_dist_kernel_version}-generic"
+            $image = "linux-image-${contrail_dist_kernel_version}-generic"
+            $image_extra = "linux-image-extra-${contrail_dist_kernel_version}-generic"
+
             package { 'apparmor' : ensure => '2.7.102-0ubuntu3.10',}
             ->
             package { $headers : ensure => present, }
