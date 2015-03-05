11dcb1ff101e15a9edbc60fa063d793b1dbe54de
tsurendra <tsurendra@juniper.net>
2015-03-04 22:12:59 -0800

    one of the compute node is not reachable after provision reboot
    Closes-Bug: #1428452
    
    Explicitly specify generic version of kernels to be downloaded.
    
    Change-Id: Ib3a9db5a8a41d12da3660717b00d28142ad0bbcc

--- contrail/manifests/contrail_common.pp
+++ contrail/manifests/contrail_common.pp
@@ -30,44 +30,46 @@ define line($file, $line, $ensure = 'present') {
 define upgrade-kernel($contrail_kernel_version) {
     $headers = "linux-headers-${contrail_kernel_version}"
     $headers_generic = "linux-headers-${contrail_kernel_version}-generic"
-    $image = "linux-image-${contrail_kernel_version}"
+    $image = "linux-image-${contrail_kernel_version}-generic"
+    $image_extra = "linux-image-extra-${contrail_kernel_version}-generic"
 
     if ($operatingsystem == "Ubuntu")
     {
         if ($lsbdistrelease == "14.04") {
-	    package { $headers : ensure => present, }
-	    ->
-	    package { $headers_generic : ensure => present, }
-	    ->
-	    package { $image : ensure => present, }
-	    ->
-	    exec { "upgrade-kernel-reboot":
-	    command => "echo upgrade-kernel-reboot >> /etc/contrail/contrail_common_exec.out && reboot ",
-		provider => shell,
-		logoutput => "true",
-		unless => ["grep -qx upgrade-kernel-reboot /etc/contrail/contrail_common_exec.out"]
-	    }
+            package { $headers : ensure => present, }
+            ->
+            package { $headers_generic : ensure => present, }
+            ->
+            package { $image : ensure => present, }
+            ->
+            package { $image_extra : ensure => present, }
+            ->
+            exec { "upgrade-kernel-reboot":
+            command => "echo upgrade-kernel-reboot >> /etc/contrail/contrail_common_exec.out && reboot ",
+                provider => shell,
+                logoutput => "true",
+                unless => ["grep -qx upgrade-kernel-reboot /etc/contrail/contrail_common_exec.out"]
+            }
         } else {
-	    package { 'apparmor' : ensure => '2.7.102-0ubuntu3.10',}
-	    ->
-	    package { $headers : ensure => present, }
-	    ->
-	    package { $headers_generic : ensure => present, }
-	    ->
-	    package { $image : ensure => present, }
-	    ->
-	    exec { "upgrade-kernel-reboot":
-	    command => "echo upgrade-kernel-reboot >> /etc/contrail/contrail_common_exec.out && reboot ",
-		provider => shell,
-		logoutput => "true",
-		unless => ["grep -qx upgrade-kernel-reboot /etc/contrail/contrail_common_exec.out"]
-	    }
+            package { 'apparmor' : ensure => '2.7.102-0ubuntu3.10',}
+            ->
+            package { $headers : ensure => present, }
+            ->
+            package { $headers_generic : ensure => present, }
+            ->
+            package { $image : ensure => present, }
+            ->
+            exec { "upgrade-kernel-reboot":
+            command => "echo upgrade-kernel-reboot >> /etc/contrail/contrail_common_exec.out && reboot ",
+                provider => shell,
+                logoutput => "true",
+                unless => ["grep -qx upgrade-kernel-reboot /etc/contrail/contrail_common_exec.out"]
+            }
         }
      } else {
         #TODO for other flavours do nothing
      }
 }
-
 #end of upgrade-kernel
 
 #source ha proxy files
