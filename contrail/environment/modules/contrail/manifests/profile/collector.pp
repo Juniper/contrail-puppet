b2d1c5d97065296740036d71c70f745fa983ca3f
Abhay Joshi <abhayj@juniper.net>
2015-03-24 17:55:39 -0700

    Partial-Bug: #1436122     Contrail puppet repo changes to go with contrail role provisioning sequencing logic from     contrail server manager. These changes, in default setting, do not cause any behavior change     in puppet catalog execution. Each role has a flag that defines if that role logic is to be     executed. By default, the flag is set to true and can be turned off from SM using hieradata     parameter setting.
    Change-Id: Ib7038ff1ec81c79067db0d00f70ddd680f05888f

--- contrail/environment/modules/contrail/manifests/profile/collector.pp
+++ contrail/environment/modules/contrail/manifests/profile/collector.pp
@@ -1,4 +1,16 @@
+# == Class: contrail::profile::collector
 # The puppet module to set up a Contrail Collector Node
-class contrail::profile::collector {
-    contain ::contrail::collector
+#
+# === Parameters:
+#
+# [*enable_module*]
+#     Flag to indicate if profile is enabled. If true, the module is invoked.
+#     (optional) - Defaults to true.
+#
+class contrail::profile::collector (
+    $enable_module = $::contrail::params::enable_collector
+) {
+    if ($enable_module) {
+        contain ::contrail::collector
+    }
 }
--- contrail/environment/modules/contrail/manifests/profile/webui.pp
+++ contrail/environment/modules/contrail/manifests/profile/webui.pp
@@ -1,4 +1,16 @@
+# == Class: contrail::profile::webui
 # The puppet module to set up a Contrail WebUI server
-class contrail::profile::webui {
-    contain ::contrail::webui
+#
+# === Parameters:
+#
+# [*enable_module*]
+#     Flag to indicate if profile is enabled. If true, the module is invoked.
+#     (optional) - Defaults to true.
+#
+class contrail::profile::webui (
+    $enable_module = $::contrail::params::enable_webui
+) {
+    if ($enable_module) {
+        contain ::contrail::webui
+    }
 }
