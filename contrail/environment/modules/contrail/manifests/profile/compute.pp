d4e10c55f9111948f8fc3f7b92dafb31c5f45598
Abhay Joshi <abhayj@juniper.net>
2015-03-24 17:55:39 -0700

    Partial-Bug: #1436122     Contrail puppet repo changes to go with contrail role provisioning sequencing logic from     contrail server manager. These changes, in default setting, do not cause any behavior change     in puppet catalog execution. Each role has a flag that defines if that role logic is to be     executed. By default, the flag is set to true and can be turned off from SM using hieradata     parameter setting.
    Change-Id: Ib7038ff1ec81c79067db0d00f70ddd680f05888f

--- contrail/environment/modules/contrail/manifests/profile/compute.pp
+++ contrail/environment/modules/contrail/manifests/profile/compute.pp
@@ -1,7 +1,20 @@
-class contrail::profile::compute {
-    require ::contrail::common
-    require ::openstack::profile::firewall
-    require ::contrail::profile::nova::compute
-#   require ::openstack::profile::ceilometer::agent
+# == Class: contrail::profile::compute
+# The puppet module to set up a Contrail compute Node
+#
+# === Parameters:
+#
+# [*enable_module*]
+#     Flag to indicate if profile is enabled. If true, the module is invoked.
+#     (optional) - Defaults to true.
+#
+class contrail::profile::compute (
+    $enable_module = $::contrail::params::enable_compute
+) {
+    if ($enable_module) {
+        require ::contrail::common
+        require ::openstack::profile::firewall
+        require ::contrail::profile::nova::compute
+        # require ::openstack::profile::ceilometer::agent
+    }
 }
 
