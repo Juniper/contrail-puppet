cdb3d933ec37b167642006ecdfa559d47cb6df68
tsurendra <tsurendra@juniper.net>
2015-03-27 15:40:11 -0700

    Closes-Bug :#1437136 SM: HA Vnc console to the instances is not working
    Proxy port was not set properly.
    Uncommented the ha-proxy stanza for vnc proxy.
    
    Change-Id: I4d0917951575caefcaef4e5ffde1eab4d10f18c2

--- contrail/environment/modules/contrail/manifests/contrail_openstack.pp
+++ contrail/environment/modules/contrail/manifests/contrail_openstack.pp
@@ -68,8 +68,17 @@ class contrail::contrail_openstack (
     $keystone_service_token = $::contrail::params::keystone_service_token,
     $keystone_auth_protocol = $::contrail::params::keystone_auth_protocol,
     $contrail_logoutput = $::contrail::params::contrail_logoutput,
+    $host_control_ip = $::contrail::params::host_ip,
 ) inherits ::contrail::params {
     # Main code for class
+
+    #slect the novncproxy based on presence of internal_vip
+    if ($internal_vip != ""){
+       $novncproxy_port = "6999"
+    } else {
+       $novncproxy_port = "5999"
+    }
+
     # Create openstackrc file.
     file { "/etc/contrail/openstackrc" :
 	ensure  => present,
@@ -96,7 +105,7 @@ class contrail::contrail_openstack (
     }
     # Set novncproxy_port to 5999, novncproxy_base_url to http://$openstack_mgmt_ip:5999/vnc_auto.html
     exec { "exec_set_novncproxy":
-        command => "openstack-config --set /etc/nova/nova.conf DEFAULT novncproxy_port 5999 && openstack-config --set /etc/nova/nova.conf DEFAULT novncproxy_base_url http://$openstack_mgmt_ip:5999/vnc_auto.html && echo exec_set_novncproxy >> /etc/contrail/contrail_openstack_exec.out",
+        command => "openstack-config --set /etc/nova/nova.conf DEFAULT novncproxy_port $novncproxy_port && openstack-config --set /etc/nova/nova.conf DEFAULT novncproxy_base_url http://$openstack_mgmt_ip:$novncproxy_port/vnc_auto.html && openstack-config --set /etc/nova/nova.conf DEFAULT novncproxy_host $openstack_mgmt_ip && echo exec_set_novncproxy >> /etc/contrail/contrail_openstack_exec.out",
         provider => shell,
         require => [ File["/etc/nova/nova.conf"] ],
         logoutput => $contrail_logoutput
