b6883886b0485865c851934503c8ae0b9e77b0e5
Abhay Joshi <abhayj@juniper.net>
2015-03-26 19:37:29 -0700

    Partial-bug: #1428189     Changes to puppet modules to move post_openstack (installing openstack supervisor)     to openstack module. This change goes along with fix for bug 1428189 in packaging.
    Change-Id: I45903b2652ae5aebe0d8bfcd493ccae88a68b186

--- contrail/environment/modules/contrail/manifests/contrail_openstack.pp
+++ contrail/environment/modules/contrail/manifests/contrail_openstack.pp
@@ -108,4 +108,25 @@ class contrail::contrail_openstack (
         require => [ File["/etc/nova/nova.conf"] ],
         logoutput => $contrail_logoutput
     }
+    package { 'contrail-openstack':
+	ensure  => present,
+    }
+    ->
+    exec { "exec_start_supervisor_openstack" :
+        command => "service supervisor-openstack start && echo start_supervisor_openstack >> /etc/contrail/contrail_openstack_exec.out",
+        unless  => "grep -qx start_supervisor_openstack /etc/contrail/contrail_openstack_exec.out",
+        provider => shell,
+        require => [ Package["contrail-openstack"]  ],
+        logoutput => $contrail_logoutput
+    }
+
+    #Make ha-mon start later
+    if($internal_vip != "") {
+        exec { "ha-mon-restart":
+            command => "service contrail-hamon restart && echo contrail-ha-mon >> /etc/contrail/contrail_openstack_exec.out",
+            provider => shell,
+            logoutput => $contrail_logoutput,
+            unless  => "grep -qx contrail-ha-mon  /etc/contrail/contrail_openstack_exec.out",
+        }
+    }
 }
--- contrail/environment/modules/contrail/manifests/profile/openstack_controller.pp
+++ contrail/environment/modules/contrail/manifests/profile/openstack_controller.pp
@@ -1,36 +1,35 @@
 # The puppet module to set up a openstack controller
 class contrail::profile::openstack_controller {
-    contain ::openstack::profile::base
-    contain ::openstack::profile::firewall
-    contain ::contrail::profile::openstack::mysql
-    contain ::openstack::profile::keystone
-    contain ::openstack::profile::memcache
-    contain ::contrail::profile::openstack::glance::api
-    contain ::openstack::profile::cinder::api
-    contain ::openstack::profile::nova::api
-    contain ::openstack::profile::horizon
-    contain ::openstack::profile::auth_file
-    contain ::openstack::profile::provision
-    Class['::openstack::profile::provision']->Service['glance-api']
-    contain ::contrail::contrail_openstack
-    Class['::openstack::profile::provision']->Class['::contrail::contrail_openstack']
+    contrail::lib::report_status { "openstack_started": state => "openstack_started" } ->
+    class {'::openstack::profile::base' : } ->
+    class {'::openstack::profile::firewall' : } ->
+    class {'::contrail::profile::openstack::mysql' : } ->
+    class {'::openstack::profile::keystone' : } ->
+    class {'::openstack::profile::memcache' : } ->
+    class {'::contrail::profile::openstack::glance::api' : } ->
+    class {'::openstack::profile::cinder::api' : } ->
+    class {'::openstack::profile::nova::api' : } ->
+    class {'::openstack::profile::horizon' : } ->
+    class {'::openstack::profile::auth_file' : } ->
+    class {'::openstack::profile::provision' : } ->
     #Contrail expects neutron to run on config nodes only
-    contain ::contrail::profile::openstack::neutron::server
+    class {'::contrail::profile::openstack::neutron::server' : } ->
 
     package { 'contrail-openstack-dashboard':
       ensure  => present,
-    }
+    } ->
 
-
-#   Though neutron runs on config, setup the db in openstack node 
+    # Though neutron runs on config, setup the db in openstack node 
     exec { 'neutron-db-sync':
-      command     => 'neutron-db-manage --config-file /etc/neutron/neutron.conf --config-file /etc/neutron/plugin.ini upgrade head',
-      path        => '/usr/bin',
-      before      => Service['neutron-server'],
-      require     => Neutron_config['database/connection'],
-      refreshonly => true
-    }
+        command     => 'neutron-db-manage --config-file /etc/neutron/neutron.conf --config-file /etc/neutron/plugin.ini upgrade head',
+        path        => '/usr/bin',
+        before      => Service['neutron-server'],
+        require     => Neutron_config['database/connection'],
+        refreshonly => true
+    } ->
+    class {'::contrail::contrail_openstack' : } ->
+    contrail::lib::report_status { "openstack_completed": state => "openstack_completed" }
 
     Class['::neutron::db::mysql'] -> Exec['neutron-db-sync']
-
+    Class['::openstack::profile::provision']->Service['glance-api']
 }
