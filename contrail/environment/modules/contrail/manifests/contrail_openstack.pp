2e6891c37c7a7968f7e6a406f3f8bf81ce177782
tsurendra <tsurendra@juniper.net>
2015-04-03 19:05:52 -0700

    Closes-Bug :#1440207 SM: On a SM provisoned cluster VNC to the Guest console doesnot work
    VNC base url was not set properly.
    Testing:
    Provisoned a HA setup and made sure console was working.
    
    Change-Id: I8ce8fcf3441a3fd95376ca0709d1a548a18864d1

--- contrail/environment/modules/contrail/manifests/contrail_openstack.pp
+++ contrail/environment/modules/contrail/manifests/contrail_openstack.pp
@@ -79,6 +79,12 @@ class contrail::contrail_openstack (
        $novncproxy_port = "5999"
     }
 
+    if ($external_vip != "") {
+       $vnc_base_url_ip = $external_vip 
+    } else {
+       $vnc_base_url_ip = $internal_vip 
+    }
+
     # Create openstackrc file.
     file { "/etc/contrail/openstackrc" :
 	ensure  => present,
@@ -105,7 +111,7 @@ class contrail::contrail_openstack (
     }
     # Set novncproxy_port to 5999, novncproxy_base_url to http://$openstack_mgmt_ip:5999/vnc_auto.html
     exec { "exec_set_novncproxy":
-        command => "openstack-config --set /etc/nova/nova.conf DEFAULT novncproxy_port $novncproxy_port && openstack-config --set /etc/nova/nova.conf DEFAULT novncproxy_base_url http://$openstack_mgmt_ip:$novncproxy_port/vnc_auto.html && openstack-config --set /etc/nova/nova.conf DEFAULT novncproxy_host $openstack_mgmt_ip && echo exec_set_novncproxy >> /etc/contrail/contrail_openstack_exec.out",
+        command => "openstack-config --set /etc/nova/nova.conf DEFAULT novncproxy_port $novncproxy_port && openstack-config --set /etc/nova/nova.conf DEFAULT novncproxy_base_url http://$vnc_base_url_ip:$vnc_base_url_port/vnc_auto.html && openstack-config --set /etc/nova/nova.conf DEFAULT novncproxy_host $openstack_ip && echo exec_set_novncproxy >> /etc/contrail/contrail_openstack_exec.out",
         provider => shell,
         require => [ File["/etc/nova/nova.conf"] ],
         logoutput => $contrail_logoutput
