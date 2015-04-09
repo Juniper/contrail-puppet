ed7713401f49dda8040ecdf9593438c9e6e384b3
tsurendra <tsurendra@juniper.net>
2015-04-09 15:08:32 -0700

    Closes-Bug :#1441813 Openstack HA SM : when mysql service is stopped on one node , all queries to mysql db are failed
    Issue:
    For mysql service failure case,
    mysql connection string points to vip:local_port.
    as mysql service only is made down keystone and other connection to
    mysql will fail.
    
    mysql connection string should ideally point to vip:haproxy_port,
    because of some issue, this is not supported,
    and in fab mysql connection string points to vip:local_port.
    
    Fix:
    After Provision is complete set the mysql string,
    as in smgr provision galera cluster is formed after openstack provision
    is done.
    
    Testing:
    Provisoned on a HA-setup and verfied the config files.
    
    Change-Id: If7b69258abc00f7d42bdaf88024c0b83b056340a

--- contrail/environment/modules/contrail/manifests/lib/post_openstack.pp
+++ contrail/environment/modules/contrail/manifests/lib/post_openstack.pp
@@ -20,12 +20,36 @@ define contrail::lib::post_openstack(
 
       #Make ha-mon start later
       if($internal_vip != "") {
+            $password = $::openstack::config::mysql_service_password
             exec { "ha-mon-restart":
                 command => "service contrail-hamon restart && echo contrail-ha-mon >> /etc/contrail/contrail_openstack_exec.out",
                 provider => shell,
                 logoutput => $contrail_logoutput,
                 unless  => "grep -qx contrail-ha-mon  /etc/contrail/contrail_openstack_exec.out",
             }
+
+           # Set mysql cpnnection string at the end
+           # as setting before will result in provision failure.
+           # Openstack is setup before galera is setup.
+           # Intiailly mysql connection string is setup to internal_vip:3306,
+           # making all mysql commands to land on vip node.
+           # Later mysql needs to change to localip to support failover scenarios.
+           # If mysql connection string is setup to local_ip while provisoning openstack.
+           # openstack 2,3 provision will fail as db-sync is done only on 1, 
+           # and they dont find the tables.
+
+	    exec { "exec_set_mysql":
+		    command => "openstack-config --set /etc/keystone/keystone.conf DATABASE connection mysql://keystone:$password@$host_control_ip/keystone  && 
+                           openstack-config --set /etc/keystone/keystone.conf mysql connection mysql://keystone:$password@$host_control_ip/keystone && 
+                           openstack-config --set /etc/cinder/cinder.conf DATABASE connection mysql://cinder:$password@$host_control_ip/cinder && 
+                           openstack-config --set /etc/glance/glance-registry.conf mysql connection mysql://glance:$password@$host_control_ip/glance && 
+                           openstack-config --set /etc/glance/glance-api.conf mysql connection mysql://glance:$password@$host_control_ip/glance && 
+                           openstack-config --set /etc/neutron/neutron.conf mysql connection mysql://neutron:$password@$host_control_ip/neutron && 
+                           echo exec_set_mysql >> /etc/contrail/contrail_openstack_exec.out",
+		    provider => shell,
+		    logoutput => $contrail_logoutput
+	    }
+
       }
 
     }
