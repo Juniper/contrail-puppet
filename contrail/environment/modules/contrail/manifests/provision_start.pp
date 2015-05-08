6e3bb78b6be618b28a552cd08cc18dd4b650034f
Abhay Joshi <abhayj@juniper.net>
2015-05-07 20:58:14 -0700

    Partial-Bug: #1452088     Change to send "provision_started" to server manager only in the beginning     of provisioning, instead of sending at every catalog run that executes     only a single step (when sequencing is enabled).
    Change-Id: I3a4a937660909c04065a4d07fa7ad45cb7e082b7

--- contrail/environment/modules/contrail/manifests/provision_start.pp
+++ contrail/environment/modules/contrail/manifests/provision_start.pp
@@ -1,12 +1,22 @@
+# == Class: contrail::provision_start
+# The puppet module to send provision started status to server manager.
+#
+# === Parameters:
+#
+# [*enable_module*]
+#     Flag to indicate if profile is enabled. If true, the module is invoked.
+#     (optional) - Defaults to true.
+#
 class contrail::provision_start(
     $state = undef,
     $contrail_logoutput = $::contrail::params::contrail_logoutput,
-)
-{
-    contrail::lib::report_status { $state:
-        state => $state, 
-        contrail_logoutput => $contrail_logoutput }
-
+    $enable_module = $::contrail::params::enable_provision_started
+) inherits ::contrail::params {
+    if ($enable_module) {
+        contrail::lib::report_status { $state:
+            state => $state, 
+            contrail_logoutput => $contrail_logoutput }
+    }
 }
 
 
