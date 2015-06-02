f64cbfda128ae303b2e3e488ae79144d739b93db
nitishkrishna <nitishk@juniper.net>
2015-06-02 12:03:46 -0700

    Closes-Bug: #1451527 - [INCOMPLETE - DO NOT MERGE] Provision Ceilometer using Smgr
    Patch 1:
    - New contrail param - enable_ceilometer
    - /etc/ceilometer/ceilometer.conf correctly configured on openstack & compute nodes
    - Mongodb installed on database nodes
    
    Patch 2:
    - Review comments on Patch 1
    - Moved database mongodb setup to new file: contrail:profile:mongodb
    
    Patch 3:
    - Configured all remaining files except keystone (bug) and pipeline.yaml
    
    Remaining:
    - Configuring the following files for ceilometer:
    /etc/keystone/keystone.conf
    /etc/ceilometer/pipeline.yaml
    
    - MongoDb Replicaset config [mongodb_replset]
    
    Change-Id: I321305ee1140546ebea8a818ca5bfba426e0c21d

--- contrail/environment/modules/contrail/manifests/profile/database.pp
+++ contrail/environment/modules/contrail/manifests/profile/database.pp
@@ -7,10 +7,18 @@
 #     Flag to indicate if profile is enabled. If true, the module is invoked.
 #     (optional) - Defaults to true.
 #
+# [*enable_ceilometer*]
+#     Flag to include or exclude ceilometer service as part of openstack module dynamically.
+#     (optional) - Defaults to false.
+#
 class contrail::profile::database (
-    $enable_module = $::contrail::params::enable_database
+    $enable_module = $::contrail::params::enable_database,
+    $enable_ceilometer = $::contrail::params::enable_ceilometer
 ) {
     if ($enable_module) {
         contain ::contrail::database
+        if ($enable_ceilometer) {
+            include ::contrail::profile::mongodb
+        }
     }
 }
