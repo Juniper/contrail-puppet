6145319bd6fc4233cfbb437dceba527003b9afd0
Abhay Joshi <abhayj@juniper.net>
2015-07-01 14:25:55 -0700

    Closes-Bug: #1470639     Remove execution of setup.sh from fab that sets up local repo for contrail packages. Instead     just install the needed packages alone from repo setup on server manager machine.
    Change-Id: I31e5f3be6399793353bf8be74afc4efe1c3f49d5

--- contrail/environment/modules/contrail/manifests/lib/contrail-install-repo.pp
+++ contrail/environment/modules/contrail/manifests/lib/contrail-install-repo.pp
@@ -3,23 +3,27 @@ define contrail::lib::contrail-install-repo(
     ) {
     $contrail_repo_type = $name
 
-    if($contrail_repo_type == "contrail-ubuntu-package") {
-	$setup_script =  "./setup.sh && echo exec-contrail-setup-$contrail_repo_type-sh >> exec-contrail-setup-sh.out"
-	$package_name = "contrail-install-packages"
-    } elsif ($contrail_repo_type == "contrail-centos-repo") {
-	$setup_script =  "./setup.sh && echo exec-contrail-setup-$contrail_repo_type-sh >> exec-contrail-setup-sh.out"
+    if (($contrail_repo_type == "contrail-ubuntu-package") or
+        ($contrail_repo_type == "contrail-centos-repo")) {
 	$package_name = "contrail-install-packages"
     }
-
+    else {
+        $package_name = ''
+    }
 
     if ( $package_name != '' ) {
-        package {$package_name: ensure => latest, install_options => '--force-yes'}
+        package {$package_name: ensure => latest, install_options => '--force-yes'} ->
+        package { ['binutils', 'make', 'libdpkg-perl', 'patch', 'dpkg-dev',
+                   'python-software-properties', 'contrail-fabric-utils', 'contrail-setup' ] :
+            ensure => latest
+        } ->
+        # May need to install fabric-utils here. below commented out code is kept for reference, in case needed.
+        # pip install --upgrade --no-deps --index-url='' /opt/contrail/python_packages/Fabric-*.tar.gz
 
-        exec { "exec-contrail-setup-$contrail_repo_type-sh" :
-    	    command => $setup_script,
-	    cwd => "/opt/contrail/contrail_packages",
-	    require => Package[$package_name],
-	    unless  => "grep -qx exec-contrail-setup-$contrail_repo_type-sh /opt/contrail/contrail_packages/exec-contrail-setup-sh.out",
+        # disabled sun-java-jre and sun-java-bin prompt during installation, add oracle license acceptance in debconf
+        # disable prompts during java installation and oracle license acceptance
+        exec { "exec-disable-jre-prompts" :
+    	    command => "echo 'sun-java6-plugin shared/accepted-sun-dlj-v1-1 boolean true' | /usr/bin/debconf-set-selections; echo 'sun-java6-bin shared/accepted-sun-dlj-v1-1 boolean true' | /usr/bin/debconf-set-selections; echo 'sun-java6-jre shared/accepted-sun-dlj-v1-1 boolean true' | /usr/bin/debconf-set-selections; echo 'debconf shared/accepted-oracle-license-v1-1 select true' | sudo debconf-set-selections; echo 'debconf shared/accepted-oracle-license-v1-1 seen true' | sudo debconf-set-selections",
 	    provider => shell,
 	    logoutput => $contrail_logoutput
         }
