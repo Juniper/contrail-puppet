a24a8f9783dfb8cf26a2fa16ca7fce073555a83a
Dheeraj Gautam <dgautam@juniper.net>
2015-07-08 03:21:36 -0700

    SM-Quench: Added unit test framework (rspec and rspec-puppet based)
    Change-Id: I003345d94c4e5b24a623c450214205e50cbd4836
    Partial-Bug: #1472554

---
+++ contrail/environment/modules/contrail/spec/classes/init_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+require 'hiera'
+require 'pp'
+
+
+hiera_file = 'spec/fixtures/hiera/hiera.yaml'   # <- required to find hiera configuration file
+
+#describe 'contrail::common' do
+describe 'contrail' do
+  context 'The following classes should be present in the catalog' do
+    let (:hiera_config) { hiera_file }
+
+    ## Below lines are for debugging purpose
+    #hiera = Hiera.new(:config => hiera_file)                        # <- use Hiera ruby class
+    #database_nlist = hiera.lookup('contrail::params::database_name_list', nil, nil)         # <- do hiera lookup
+    #config_nlist = hiera.lookup('contrail::params::config_name_list', nil, nil)         # <- do hiera lookup
+    #control_nlist = hiera.lookup('contrail::params::control_name_list', nil, nil)         # <- do hiera lookup
+    #compute_nlist = hiera.lookup('contrail::params::compute_name_list', nil, nil)         # <- do hiera lookup
+    #collector_nlist = hiera.lookup('contrail::params::collector_name_list', nil, nil)         # <- do hiera lookup
+    #host_ip = hiera.lookup('contrail::params::host_ip', nil, nil)         # <- do hiera lookup
+    #openstack_iplist = hiera.lookup('contrail::params::openstack_ip_list', nil, nil)         # <- do hiera lookup
+    #compute_iplist = hiera.lookup('contrail::params::compute_ip_list', nil, nil)         # <- do hiera lookup
+    #webui_iplist = hiera.lookup('contrail::params::webui_ip_list', nil, nil)         # <- do hiera lookup
+    #control_iplist = hiera.lookup('contrail::params::control_ip_list', nil, nil)         # <- do hiera lookup
+    #config_iplist = hiera.lookup('contrail::params::config_ip_list', nil, nil)         # <- do hiera lookup
+    #collector_iplist = hiera.lookup('contrail::params::collector_ip_list', nil, nil)         # <- do hiera lookup
+    #repo_name = hiera.lookup('contrail::params::contrail_repo_name', nil, nil)         # <- do hiera lookup
+    #pp database_nlist, host_ip, repo_name
+
+    #let (:hiera_data) { {
+        #:'config_name_list' => config_nlist,
+        #:'database_name_list' => database_nlist,
+        #:'control_name_list' => control_nlist,
+        #:'compute_name_list' => compute_nlist ,
+        #:'collector_name_list' => collector_nlist,
+    #} }
+    it { should compile }                # this is the test to check if it compiles.
+  end
+end
