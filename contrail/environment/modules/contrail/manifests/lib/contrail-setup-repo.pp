270c08147805997265ebb666c058b306d32b2b1c
Dheeraj Gautam <dgautam@juniper.net>
2015-07-14 01:12:56 -0700

    SM-quench: Errors/warnings reported by puppet-lint
    Partial-Bug: #1472554
    
    Conflicts:
    	contrail/environment/modules/contrail/manifests/lib/contrail-install-repo.pp
    
    Change-Id: I06a5fc76d8f75affab48f7ef776f24b70c688f2c

--- contrail/environment/modules/contrail/manifests/lib/contrail-setup-repo.pp
+++ contrail/environment/modules/contrail/manifests/lib/contrail-setup-repo.pp
@@ -2,22 +2,22 @@ define contrail::lib::contrail-setup-repo(
     $contrail_repo_ip,
     $contrail_logoutput = false,
 ) {
-    $contrail_repo_name = $name
-    if ($operatingsystem == "Centos" or $operatingsystem == "Fedora") {
-	file { "/etc/yum.repos.d/cobbler-config.repo" :
-	    ensure  => present,
-	    content => template("contrail-common/contrail-yum-repo.erb")
-	}
+  $contrail_repo_name = $name
+  if ($::operatingsystem == 'Centos' or $::operatingsystem == 'Fedora') {
+    file { '/etc/yum.repos.d/cobbler-config.repo' :
+      ensure  => present,
+      content => template('contrail-common/contrail-yum-repo.erb')
     }
-    if ($operatingsystem == "Ubuntu") {
-	$pattern1 = "deb http:\/\/$contrail_repo_ip\/contrail\/repo\/$contrail_repo_name .\/"
-	$pattern2 = "deb http://$contrail_repo_ip/contrail/repo/$contrail_repo_name ./"
-	$repo_cfg_file = "/etc/apt/sources.list"
-	exec { "update-sources-list-$contrail_repo_name" :
-	    command   => "sed -i \"/$pattern1/d\" $repo_cfg_file && echo \"$pattern2\"|cat - $repo_cfg_file > /tmp/out && mv /tmp/out $repo_cfg_file && apt-get update",
-	    unless  => "head -1 $repo_cfg_file | grep -qx \"$pattern2\"",
-	    provider => shell,
-	    logoutput => $contrail_logoutput
-	}
-    }
+  }
+  if ($::operatingsystem == 'Ubuntu') {
+      $pattern1 = "deb http:\/\/${contrail_repo_ip}\/contrail\/repo\/${contrail_repo_name} .\/"
+      $pattern2 = "deb http://${contrail_repo_ip}/contrail/repo/${contrail_repo_name} ./"
+      $repo_cfg_file = '/etc/apt/sources.list'
+      exec { "update-sources-list-${contrail_repo_name}" :
+          command   => "sed -i \"/${pattern1}/d\" ${repo_cfg_file} && echo \"${pattern2}\"|cat - ${repo_cfg_file} > /tmp/out && mv /tmp/out ${repo_cfg_file} && apt-get update",
+          unless    => "head -1 ${repo_cfg_file} | grep -qx \"${pattern2}\"",
+          provider  => shell,
+          logoutput => $contrail_logoutput
+      }
+  }
 }
--- contrail/environment/modules/contrail/manifests/lib/create-interface-cb.pp
+++ contrail/environment/modules/contrail/manifests/lib/create-interface-cb.pp
@@ -1,10 +1,12 @@
+## TODO: Change function name to avoid '-; hyphen name function name
+
 define contrail::lib::create-interface-cb(
-    $contrail_package_id,
-    $contrail_logoutput = false,
+  $contrail_package_id,
+  $contrail_logoutput = false,
 ) {
-    exec { "contrail-interface-cb" :
-        command => "curl -H \"Content-Type: application/json\" -d '{\"package_image_id\":\"$contrail_package_id\",\"id\":\"$hostname\"}' http://$serverip:9001/interface_created && echo create-interface-cb >> /etc/contrail/contrail_common_exec.out",
-        provider => shell,
-        logoutput => $contrail_logoutput
-    }
+  exec { 'contrail-interface-cb' :
+    command   => "curl -H \"Content-Type: application/json\" -d '{\"package_image_id\":\"${contrail_package_id}\",\"id\":\"${::hostname}\"}' http://${::serverip}:9001/interface_created && echo create-interface-cb >> /etc/contrail/contrail_common_exec.out",
+    provider  => shell,
+    logoutput => $contrail_logoutput
+  }
 }
