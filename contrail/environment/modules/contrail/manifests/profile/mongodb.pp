7d3d392a6f4a8fd8b2ca4f19cb77c78d3a1d721c
Dheeraj Gautam <dgautam@juniper.net>
2015-07-14 03:06:50 -0700

    SM-Quench: 2nd round of checking for cleanup
    Change-Id: I4bd9ec9003cc6e34c8034fb99c6118b535b79e6e
    Partial-Bug: #1472554

--- contrail/environment/modules/contrail/manifests/profile/mongodb.pp
+++ contrail/environment/modules/contrail/manifests/profile/mongodb.pp
@@ -9,28 +9,28 @@ class contrail::profile::mongodb {
       # Mongo DB Replset members are primary_db + slave_members below - All database nodes
       $mongo_slave_ip_list_str = inline_template('<%= @database_ip_list.delete_if {|x| x == @primary_db_ip }.join(";") %>')
       $mongo_slave_ip_list = split($mongo_slave_ip_list_str, ';')
-      notify { "contrail::profile::mongodb - mongo_slave_ip_list = $mongo_slave_ip_list":;}
+      notify { "contrail::profile::mongodb - mongo_slave_ip_list = ${mongo_slave_ip_list}":;}
       $ceilometer_mongo_password = hiera(openstack::ceilometer::mongo::password)
       $ceilometer_password = hiera(openstack::ceilometer::password)
       $ceilometer_meteringsecret = hiera(openstack::ceilometer::meteringsecret)
       $mongodb_bind_address = $contrail::params::host_non_mgmt_ip
-      $port = ":27017"
+      $port = ':27017'
       $contrail_logoutput = $::contrail::params::contrail_logoutput
 
-      define add_rs_members ($primary_db_ip){
+      define add_rs_members ($primary_db_ip) {
         # Mongo DB Add RS members
-        exec { "exec_mongo_add_rs_member $name":
-          command => "/usr/bin/mongo --host $primary_db_ip --quiet --eval \'rs.add(\"$name:27017\").ok\' && echo \"exec_mongo_add_rs_member $name\" >> /etc/contrail/contrail_database_exec.out",
+        exec { "exec_mongo_add_rs_member ${name}":
+          command   => "/usr/bin/mongo --host ${primary_db_ip} --quiet --eval \'rs.add(\"${name}:27017\").ok\' && echo \"exec_mongo_add_rs_member ${name}\" >> /etc/contrail/contrail_database_exec.out",
           logoutput => $contrail_logoutput,
-          returns => 0,
-          unless  => "/bin/grep -qx \"exec_mongo_add_rs_member $name\" /etc/contrail/contrail_database_exec.out",
+          returns   => 0,
+          unless    => "/bin/grep -qx \"exec_mongo_add_rs_member ${name}\" /etc/contrail/contrail_database_exec.out",
         }
       }
 
       class { '::mongodb::server':
             bind_ip => ['127.0.0.1', $mongodb_bind_address],
             replset => 'rs-ceilometer',
-            master => true,
+            master  => true,
       }
 
       class { '::mongodb::client': }
@@ -43,27 +43,27 @@ class contrail::profile::mongodb {
 
       if($mongodb_bind_address == $primary_db_ip){
         # Check Mongodb conection
-        exec { "exec_mongo_connection":
-            command => "/usr/bin/mongo --host $primary_db_ip --quiet --eval \'db = db.getSiblingDB(\"ceilometer\")\'",
+        exec { 'exec_mongo_connection':
+            command   => "/usr/bin/mongo --host ${primary_db_ip} --quiet --eval \'db = db.getSiblingDB(\"ceilometer\")\'",
             logoutput => $contrail_logoutput,
-            returns => 0,
+            returns   => 0,
         }
 
         # Setup MongoDb replicaSet
-        exec { "exec_mongo_create_replset":
-            command => "/usr/bin/mongo --host $primary_db_ip --quiet --eval \'rs.initiate({_id:\"rs-ceilometer\", members:[{_id:0, host:\"$primary_db_ip:27017\"}]}).ok\' && echo exec_mongo_create_replset >> /etc/contrail/contrail_database_exec.out",
+        exec { 'exec_mongo_create_replset':
+            command   => "/usr/bin/mongo --host ${primary_db_ip} --quiet --eval \'rs.initiate({_id:\"rs-ceilometer\", members:[{_id:0, host:\"${primary_db_ip}:27017\"}]}).ok\' && echo exec_mongo_create_replset >> /etc/contrail/contrail_database_exec.out",
             logoutput => $contrail_logoutput,
-            returns => 0,
-            tries => 5,
-            unless  => "/bin/grep -qx exec_mongo_create_replset /etc/contrail/contrail_database_exec.out",
+            returns   => 0,
+            tries     => 5,
+            unless    => '/bin/grep -qx exec_mongo_create_replset /etc/contrail/contrail_database_exec.out',
             try_sleep => 15,
         }
 
         # Check Mongodb Primary is Master
-        exec { "exec_mongo_check_master":
-            command => "/usr/bin/mongo --host $primary_db_ip --quiet --eval \'db.isMaster().ismaster\'",
+        exec { 'exec_mongo_check_master':
+            command   => "/usr/bin/mongo --host ${primary_db_ip} --quiet --eval \'db.isMaster().ismaster\'",
             logoutput => $contrail_logoutput,
-            returns => 0,
+            returns   => 0,
         }
 
         add_rs_members {
@@ -72,35 +72,35 @@ class contrail::profile::mongodb {
         }
 
         # Verify Replica set status and members
-        exec { "exec_verify_rs_status":
-            command => "/usr/bin/mongo --host $primary_db_ip --quiet --eval \'rs.status().ok\'",
+        exec { 'exec_verify_rs_status':
+            command   => "/usr/bin/mongo --host ${primary_db_ip} --quiet --eval \'rs.status().ok\'",
             logoutput => $contrail_logoutput,
-            returns => 0,
+            returns   => 0,
         }
 
         # MongoDb check user ceilometer
-        exec { "exec_check_user_ceilometer":
-            command => "/usr/bin/mongo --host $primary_db_ip --quiet --eval \'db.system.users.find({user:\"ceilometer\"}).count()\'",
+        exec { 'exec_check_user_ceilometer':
+            command   => "/usr/bin/mongo --host ${primary_db_ip} --quiet --eval \'db.system.users.find({user:\"ceilometer\"}).count()\'",
             logoutput => $contrail_logoutput,
-            returns => 0,
-            tries => 5,
+            returns   => 0,
+            tries     => 5,
             try_sleep => 15,
         }
 
         # Add MongoDb user ceilometer
-        exec { "exec_add_user_ceilometer":
-            command => "/usr/bin/mongo --host $primary_db_ip --quiet --eval \'db = db.getSiblingDB(\"ceilometer\"); db.addUser({user: \"ceilometer\", pwd: \"$ceilometer_mongo_password\", roles: [ \"readWrite\", \"dbAdmin\" ]})\' && echo exec_add_user_ceilometer >> /etc/contrail/contrail_database_exec.out",
+        exec { 'exec_add_user_ceilometer':
+            command   => "/usr/bin/mongo --host ${primary_db_ip} --quiet --eval \'db = db.getSiblingDB(\"ceilometer\"); db.addUser({user: \"ceilometer\", pwd: \"${ceilometer_mongo_password}\", roles: [ \"readWrite\", \"dbAdmin\" ]})\' && echo exec_add_user_ceilometer >> /etc/contrail/contrail_database_exec.out",
             logoutput => $contrail_logoutput,
-            returns => 0,
-            tries => 5,
+            returns   => 0,
+            tries     => 5,
             try_sleep => 15,
-            unless  => "/bin/grep -qx exec_add_user_ceilometer /etc/contrail/contrail_database_exec.out",
+            unless    => '/bin/grep -qx exec_add_user_ceilometer /etc/contrail/contrail_database_exec.out',
         }
 
       }
 
-      notify { "contrail::profile::mongodb - mongodb_bind_address = $mongodb_bind_address":;}
-      notify { "contrail::profile::mongodb - primary_db_ip = $primary_db_ip":;}
+      notify { "contrail::profile::mongodb - mongodb_bind_address = ${mongodb_bind_address}":;}
+      notify { "contrail::profile::mongodb - {primary_db_ip} = ${primary_db_ip}":;}
 
       Class['::mongodb::server'] -> Class['::mongodb::client']
 }
--- contrail/environment/modules/contrail/manifests/profile/tsn.pp
+++ contrail/environment/modules/contrail/manifests/profile/tsn.pp
@@ -1,10 +1,10 @@
 class contrail::profile::tsn(
-    $enable_tsn= $::contrail::params::enable_tsn
+  $enable_tsn= $::contrail::params::enable_tsn
 )
 {
-        notify {"*** profile tsn *** $enable_tsn":}
-    if ($enable_tsn)  {
-        notify {"*** profile tsn enabled ***":;}
-        contain ::contrail::tsn
-    }
+  notify {"*** profile tsn *** ${enable_tsn}":}
+  if ($enable_tsn)  {
+    notify {'*** profile tsn enabled ***':;}
+    contain ::contrail::tsn
+  }
 }
