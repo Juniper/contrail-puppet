24877793bcb2b968a5f3c3ff1f001671a9ef0583
Dheeraj Gautam <dgautam@juniper.net>
2015-07-14 03:06:50 -0700

    SM-Quench: 2nd round of checking for cleanup
    Change-Id: I4bd9ec9003cc6e34c8034fb99c6118b535b79e6e
    Partial-Bug: #1472554

--- contrail/environment/modules/contrail/manifests/profile/nova/compute.pp
+++ contrail/environment/modules/contrail/manifests/profile/nova/compute.pp
@@ -3,6 +3,6 @@ class contrail::profile::nova::compute {
     #include ::openstack::common::neutron
 
     $controller_management_address = hiera(openstack::controller::address::management)
-    notify { "contrail::profile::nova::compute - controller_management_address = $controller_management_address":; }
+    notify { "contrail::profile::nova::compute - controller_management_address = ${controller_management_address}":; }
     include contrail::compute
 }
