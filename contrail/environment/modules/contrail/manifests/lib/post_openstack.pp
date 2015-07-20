b531a83ca0f2afe83aa214486426ab163f5550f2
Dheeraj Gautam <dgautam@juniper.net>
2015-07-20 12:44:26 -0700

    SM-Quench: Fix issue that broke mainline
    Change-Id: I31bcb29f2cb754044760b96570f6d3a89aff3915
    Partial-Bug: #1472554

--- contrail/environment/modules/contrail/manifests/lib/post_openstack.pp
+++ contrail/environment/modules/contrail/manifests/lib/post_openstack.pp
@@ -19,39 +19,40 @@ define contrail::lib::post_openstack(
 
   #Make ha-mon start later
   if($internal_vip != "") {
-    #Get the value for hiera and not from openstack::config
-    #as with sequencing changes openstack modules is disabled after its
-    #step is completed.
+      #Get the value for hiera and not from openstack::config
+      #as with sequencing changes openstack modules is disabled after its
+      #step is completed.
 
-    $password = hiera(openstack::mysql::service_password)
-    exec { "ha-mon-restart":
-       command   => "service contrail-hamon restart && echo contrail-ha-mon >> /etc/contrail/contrail_openstack_exec.out",
-       provider  => shell,
-       before    => "Exec[exec_start_supervisor_openstack]",
-       logoutput => $contrail_logoutput,
-       unless    => "grep -qx contrail-ha-mon  /etc/contrail/contrail_openstack_exec.out",
-    }
-    # Set mysql cpnnection string at the end
-    # as setting before will result in provision failure.
-    # Openstack is setup before galera is setup.
-    # Intiailly mysql connection string is setup to internal_vip:3306,
-    # making all mysql commands to land on vip node.
-    # Later mysql needs to change to localip to support failover scenarios.
-    # If mysql connection string is setup to local_ip while provisoning openstack.
-    # openstack 2,3 provision will fail as db-sync is done only on 1, 
-    # and they dont find the tables.
+      $password = hiera(openstack::mysql::service_password)
+      exec { "ha-mon-restart":
+         command   => "service contrail-hamon restart && echo contrail-ha-mon >> /etc/contrail/contrail_openstack_exec.out",
+         provider  => shell,
+         before    => "Exec[exec_start_supervisor_openstack]",
+         logoutput => $contrail_logoutput,
+         unless    => "grep -qx contrail-ha-mon  /etc/contrail/contrail_openstack_exec.out",
+      }
+      # Set mysql cpnnection string at the end
+      # as setting before will result in provision failure.
+      # Openstack is setup before galera is setup.
+      # Intiailly mysql connection string is setup to internal_vip:3306,
+      # making all mysql commands to land on vip node.
+      # Later mysql needs to change to localip to support failover scenarios.
+      # If mysql connection string is setup to local_ip while provisoning openstack.
+      # openstack 2,3 provision will fail as db-sync is done only on 1, 
+      # and they dont find the tables.
 
-    exec { 'exec_set_mysql':
-      command   => "openstack-config --set /etc/keystone/keystone.conf database connection mysql://keystone:${password}@${host_control_ip/}keystone  && 
-               openstack-config --set /etc/keystone/keystone.conf sql connection mysql://keystone:${password}@${host_control_ip}/keystone && 
-               openstack-config --set /etc/cinder/cinder.conf database connection mysql://cinder:${password}@${host_control_ip}/cinder && 
-               openstack-config --set /etc/glance/glance-registry.conf database connection mysql://glance:${password}@${host_control_ip}/glance && 
-               openstack-config --set /etc/glance/glance-api.conf database connection mysql://glance:${password}@${host_control_ip}/glance && 
-               openstack-config --set /etc/neutron/neutron.conf database connection mysql://neutron:${password}@${host_control_ip}/neutron && 
-               echo exec_set_mysql >> /etc/contrail/contrail_openstack_exec.out",
-      provider  => shell,
-      before    => Exec['exec_start_supervisor_openstack'],
-      logoutput => $contrail_logoutput
+      exec { 'exec_set_mysql':
+        command   => "openstack-config --set /etc/keystone/keystone.conf database connection mysql://keystone:${password}@${host_control_ip}/keystone  && 
+                 openstack-config --set /etc/keystone/keystone.conf sql connection mysql://keystone:${password}@${host_control_ip}/keystone && 
+                 openstack-config --set /etc/cinder/cinder.conf database connection mysql://cinder:${password}@${host_control_ip}/cinder && 
+                 openstack-config --set /etc/glance/glance-registry.conf database connection mysql://glance:${password}@${host_control_ip}/glance && 
+                 openstack-config --set /etc/glance/glance-api.conf database connection mysql://glance:${password}@${host_control_ip}/glance && 
+                 openstack-config --set /etc/neutron/neutron.conf database connection mysql://neutron:${password}@${host_control_ip}/neutron && 
+                 echo exec_set_mysql >> /etc/contrail/contrail_openstack_exec.out",
+        provider  => shell,
+        before    => Exec['exec_start_supervisor_openstack'],
+        logoutput => $contrail_logoutput
+      }
     }
   }
 }
--- contrail/environment/modules/contrail/manifests/lib/report_status.pp
+++ contrail/environment/modules/contrail/manifests/lib/report_status.pp
@@ -3,7 +3,7 @@ define contrail::lib::report_status(
     $contrail_logoutput = false,
 ) {
   exec { "contrail-status-${state}" :
-    command   => "mkdir -p /etc/contrail/ && wget --post-data=\"\" \"http://puppet:9002/server_status?server_id=${::hostname}&state=${state} \" && echo contrail-status-${state} >> /etc/contrail/contrail_common_exec.out",
+    command   => "mkdir -p /etc/contrail/ && wget --post-data=\"\" \"http://puppet:9002/server_status?server_id=${::hostname}&state=${state}\" && echo contrail-status-${state} >> /etc/contrail/contrail_common_exec.out",
     provider  => shell,
     logoutput => $contrail_logoutput
   }
