8d242b7ab9384fa74a1255d07bc0e0c22c567f7b
nitishkrishna <nitishk@juniper.net>
2015-08-13 12:19:55 -0700

    Related-Bug: #1471596 - Removing Fab Commands from Server Manager Provisioning
    This check-in:
    Removes all fab commands that galera setup script was calling
    Necessary as fab was removed from SM provisioning
    
    Change-Id: Ib289948e0e0317cff1a9de064e86b22b6acca265

--- contrail/environment/modules/contrail/files/galera_setup_gcomm_mod.py
+++ contrail/environment/modules/contrail/files/galera_setup_gcomm_mod.py
@@ -9,10 +9,8 @@ import argparse
 import ConfigParser
 import time
 import commands
-from fabric.api import local,run
-from fabric.context_managers import lcd, hide, settings
-from fabric.operations import get, put
 
+import subprocess
 
 from contrail_provisioning.common.base import ContrailSetup
 from contrail_provisioning.openstack.ha.templates import galera_param_template
@@ -47,6 +45,31 @@ class GaleraSetup(ContrailSetup):
         else:
             return False
 
+    def exec_command(cmd, ignore_failure = False, show_output = True, capture_output = False):
+
+        returncode = False
+        output = None
+        try:
+            if show_output:
+                ret = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell = True)
+            else:
+                FNULL = open(os.devnull, 'w')
+                ret = subprocess.Popen(cmd, stdout = FNULL, shell = True)
+
+            if capture_output:
+                output = ret.communicate()[0]
+            returncode = ret.returncode
+
+        except Exception as e:
+            if ignore_failure:
+                return (output, returncode)
+            raise e
+
+        if returncode == 0:
+            return output, True
+        else:
+            return output, False
+
     def parse_args(self, args_str):
         '''
         Eg. setup-vnc-galera --openstack0_user root --openstack0_password c0ntrail123 --self_ip 10.1.5.11
@@ -75,13 +98,13 @@ class GaleraSetup(ContrailSetup):
     def fixup_config_files(self):
         if self.check_cluster(self._args.galera_ip_list, self._args.self_ip):
             return
-        with settings(warn_only=True):
-            local("service contrail-hamon stop")
-            local("service cmon stop")
-            local("service mysql stop")
-            local("rm -rf /var/lib/mysql/grastate.dat")
-            local("rm -rf /var/lib/mysql/galera.cache")
-            self.cleanup_redo_log()
+
+        self.exec_command("service contrail-hamon stop", ignore_failure = True)
+        self.exec_command("service cmon stop", ignore_failure = True)
+        self.exec_command("service mysql stop", ignore_failure = True)
+        self.exec_command("rm -rf /var/lib/mysql/grastate.dat", ignore_failure = True)
+        self.exec_command("rm -rf /var/lib/mysql/galera.cache", ignore_failure = True)
+        self.cleanup_redo_log()
 
         # fix galera_param
         template_vals = {'__mysql_host__' : self._args.self_ip,
@@ -90,7 +113,7 @@ class GaleraSetup(ContrailSetup):
         self._template_substitute_write(galera_param_template.template,
                                         template_vals,
                                         self._temp_dir_name + '/galera_param')
-        local("sudo mv %s/galera_param /etc/contrail/ha/" % (self._temp_dir_name))
+        self.exec_command("sudo mv %s/galera_param /etc/contrail/ha/" % (self._temp_dir_name))
         zk_servers_ports = ','.join(['%s:2181' %(s) for s in self._args.zoo_ip_list])
 
         # fix cmon_param
@@ -109,11 +132,11 @@ class GaleraSetup(ContrailSetup):
         self._template_substitute_write(cmon_param_template.template,
                                         template_vals,
                                         self._temp_dir_name + '/cmon_param')
-        local("sudo mv %s/cmon_param /etc/contrail/ha/" % (self._temp_dir_name))
+        self.exec_command("sudo mv %s/cmon_param /etc/contrail/ha/" % (self._temp_dir_name))
 
-        local("echo %s > /etc/contrail/galeraid" % self._args.openstack_index)
+        self.exec_command("echo %s > /etc/contrail/galeraid" % self._args.openstack_index)
         if self.pdist in ['Ubuntu']:
-            local("ln -sf /bin/true /sbin/chkconfig")
+            self.exec_command("ln -sf /bin/true /sbin/chkconfig")
             self.mysql_svc = 'mysql'
             self.mysql_conf = '/etc/mysql/my.cnf'
             wsrep_conf = '/etc/mysql/conf.d/wsrep.cnf'
@@ -168,7 +191,7 @@ class GaleraSetup(ContrailSetup):
                         }
         self._template_substitute_write(wsrep_template, template_vals,
                                 self._temp_dir_name + '/%s' % wsrep_conf_file)
-        local("sudo mv %s/%s %s" % (self._temp_dir_name, wsrep_conf_file,
+        self.exec_command("sudo mv %s/%s %s" % (self._temp_dir_name, wsrep_conf_file,
                                     wsrep_conf))
 #        if self._args.openstack_index == 1:
 #            local('sed -ibak "s#wsrep_cluster_address=.*#wsrep_cluster_address=gcomm://#g" %s' % (wsrep_conf))
@@ -179,87 +202,84 @@ class GaleraSetup(ContrailSetup):
                         }
         self._template_substitute_write(cmon_conf_template.template, template_vals,
                                         self._temp_dir_name + '/cmon.cnf')
-        local("sudo mv %s/cmon.cnf /etc/cmon.cnf" % (self._temp_dir_name))
+        self.exec_command("sudo mv %s/cmon.cnf /etc/cmon.cnf" % (self._temp_dir_name))
 
     def install_mysql_db(self):
-        local('chkconfig %s on' % self.mysql_svc)
-        local('chown -R mysql:mysql /var/lib/mysql/')
-        with settings(warn_only=True):
-            install_db = local("service %s restart" % self.mysql_svc).failed
+        self.exec_command('chkconfig %s on' % self.mysql_svc)
+        self.exec_command('chown -R mysql:mysql /var/lib/mysql/')
+        install_db = exec_command("service %s restart" % self.mysql_svc, ignore_failure = True)[1]
         if install_db:
-            local('mysql_install_db --user=mysql --ldata=/var/lib/mysql')
+            exec_command('mysql_install_db --user=mysql --ldata=/var/lib/mysql')
             self.cleanup_redo_log()
-            local("service %s restart" % self.mysql_svc)
+            exec_command("service %s restart" % self.mysql_svc)
 
     def create_mysql_token_file(self):
         # Use MYSQL_ROOT_PW from the environment or generate a new password
         if os.path.isfile(self.mysql_token_file):
-            self.mysql_token = local('cat %s' % self.mysql_token_file, capture=True).strip()
+            self.mysql_token = self.exec_command('cat %s' % self.mysql_token_file, capture_output = True)[0].strip()
         else:
             if os.environ.get('MYSQL_ROOT_PW'):
                 self.mysql_token = os.environ.get('MYSQL_ROOT_PW')
             else:
-                 self.mysql_token = local('openssl rand -hex 10', capture=True).strip()
-            local("echo %s > %s" % (self.mysql_token, self.mysql_token_file))
-            local("chmod 400 %s" % self.mysql_token_file)
+                 self.mysql_token = self.exec_command('openssl rand -hex 10', capture_output=True)[0].strip()
+            self.exec_command("echo %s > %s" % (self.mysql_token, self.mysql_token_file))
+            self.exec_command("chmod 400 %s" % self.mysql_token_file)
 
     def get_mysql_token_file(self):
         retries = 10
         if not os.path.isfile(self.mysql_token_file):
             while retries:
-                with settings(host_string = '%s@%s' % (self._args.openstack0_user, self._args.galera_ip_list[0]),
-                              password=self._args.openstack0_passwd, warn_only=True):
-                    if get('%s' % self.mysql_token_file, '/etc/contrail/').failed:
-                        time.sleep(1)
-                        retries -= 1
-                        print " Retry(%s) to get the %s." % ((5 - retries), self.mysql_token_file)
-                    else:
-                        break
-        self.mysql_token = local('cat %s' % self.mysql_token_file, capture=True).strip()
+                if self.exec_command('scp %s@%s:%s /etc/contrail/' % (self._args.openstack0_user, 
+                                   self._args.galera_ip_list[0]), ignore_failure = True)[1] == False: 
+                    time.sleep(1)
+                    retries -= 1
+                    print " Retry(%s) to get the %s." % ((5 - retries), self.mysql_token_file)
+                else:
+                    break
+        self.mysql_token = self.exec_command('cat %s' % self.mysql_token_file, capture_output=True)[0].strip()
 
     def set_mysql_root_password(self):
         # Only for the very first time
         mysql_priv_access = "mysql -uroot -e"
         # Set root password for mysql
-        with settings(warn_only=True):
-            if local('echo show databases |mysql -u root > /dev/null').succeeded:
-                local('%s "use mysql; update user set password=password(\'%s\') where user=\'root\'"' % (mysql_priv_access, self.mysql_token))
-                local('mysqladmin password %s' % self.mysql_token)
-            elif local('echo show databases |mysql -u root -p%s> /dev/null' % self.mysql_token).succeeded:
-                print "Mysql root password is already set to '%s'" % self.mysql_token
-            else:
-                raise RuntimeError("MySQL root password unknown, reset and retry")
+        if self.exec_command('echo show databases |mysql -u root > /dev/null', ignore_failure=True)[1] == True:
+            self.exec_command('%s "use mysql; update user set password=password(\'%s\') where user=\'root\'"' 
+                                        % (mysql_priv_access, self.mysql_token), ignore_failure = True)
+            self.exec_command('mysqladmin password %s' % self.mysql_token, ignore_failure = True)
+        elif self.exec_command('echo show databases |mysql -u root -p%s> /dev/null' % self.mysql_token, ignore_failure=True)[0]:
+            print "Mysql root password is already set to '%s'" % self.mysql_token
+        else:
+            raise RuntimeError("MySQL root password unknown, reset and retry")
 
     def setup_grants(self):
         mysql_cmd =  "mysql --defaults-file=%s -uroot -p%s -e" % (self.mysql_conf, self.mysql_token)
         host_list = self._args.galera_ip_list + ['localhost', '127.0.0.1']
         for host in host_list:
-            with settings(hide('everything'),warn_only=True):
-                 local('mysql -u root -p%s -e "CREATE USER \'root\'@\'%s\' IDENTIFIED BY %s"' % (self.mysql_token, host, self.mysql_token))
-            local('%s "SET WSREP_ON=0;SET SQL_LOG_BIN=0; GRANT ALL ON *.* TO root@%s IDENTIFIED BY \'%s\'"' %
-                   (mysql_cmd, host, self.mysql_token))
-        local('%s "SET wsrep_on=OFF; DELETE FROM mysql.user WHERE user=\'\'"' % mysql_cmd)
-        local('%s "FLUSH PRIVILEGES"' % mysql_cmd)
+            self.exec_command('mysql -u root -p%s -e "CREATE USER \'root\'@\'%s\' IDENTIFIED BY %s"' % 
+                               (self.mysql_token, host, self.mysql_token), ignore_failure=True, show_output=False)
+            self.exec_command('%s "SET WSREP_ON=0;SET SQL_LOG_BIN=0; GRANT ALL ON *.* TO root@%s IDENTIFIED BY \'%s\'"' %
+                               (mysql_cmd, host, self.mysql_token))
+        self.exec_command('%s "SET wsrep_on=OFF; DELETE FROM mysql.user WHERE user=\'\'"' % mysql_cmd)
+        self.exec_command('%s "FLUSH PRIVILEGES"' % mysql_cmd)
 
     def setup_cron(self):
-        with settings(hide('everything'), warn_only=True):
-            local('crontab -l > %s/galera_cron' % self._temp_dir_name)
-        local('echo "0 * * * * /opt/contrail/bin/contrail-token-clean.sh" >> %s/galera_cron' % self._temp_dir_name)
-        local('crontab %s/galera_cron' % self._temp_dir_name)
-        local('rm %s/galera_cron' % self._temp_dir_name)
+        self.exec_command('crontab -l > %s/galera_cron' % self._temp_dir_name, ignore_failure = True, show_output = False)
+        self.exec_command('echo "0 * * * * /opt/contrail/bin/contrail-token-clean.sh" >> %s/galera_cron' % self._temp_dir_name)
+        self.exec_command('crontab %s/galera_cron' % self._temp_dir_name)
+        self.exec_command('rm %s/galera_cron' % self._temp_dir_name)
 
     def run_services(self):
         if self.check_cluster(self._args.galera_ip_list, self._args.self_ip):
             return
         self.cleanup_redo_log()
         if self._args.openstack_index == 1:
-            local("service %s stop" % self.mysql_svc)
-            local("service %s start --wsrep_cluster_address=gcomm://" % self.mysql_svc)
+            self.exec_command("service %s stop" % self.mysql_svc)
+            self.exec_command("service %s start --wsrep_cluster_address=gcomm://" % self.mysql_svc)
         else:
             cmd = "mysql -h%s -uroot -p%s " % (self._args.galera_ip_list[0], self.mysql_token)
             cmd += "-e \"show global status where variable_name='wsrep_local_state'\" | awk '{print $2}' | sed '1d'"
             for i in range(10):
-                wsrep_local_state = local(cmd, capture=True).strip()
+                wsrep_local_state = self.exec_command(cmd, capture_output=True)[0].strip()
                 if wsrep_local_state == '4':
                     break
                 else:
@@ -267,18 +287,17 @@ class GaleraSetup(ContrailSetup):
                     print "Waiting for first galera node to create new cluster."
             if wsrep_local_state != '4':
                 raise RuntimeError("Unable able to bring up galera in first node, please verify and continue.")
-            local("service %s restart" % self.mysql_svc)
-        local("sudo update-rc.d -f mysql remove")
-        local("sudo update-rc.d mysql defaults")
+            self.exec_command("service %s restart" % self.mysql_svc)
+        self.exec_command("sudo update-rc.d -f mysql remove")
+        self.exec_command("sudo update-rc.d mysql defaults")
 
     def cleanup_redo_log(self):
         # Delete the default initially created redo log file
         # This is required coz the wsrep config changes the
         # size of redo log file
-        with settings(warn_only = True):
-            siz = local("ls -l /var/lib/mysql/ib_logfile1 | awk '{print $5}'", capture=True).strip()
-            if siz == self.mysql_redo_log_sz:
-                local("rm -rf /var/lib/mysql/ib_logfile*")
+        siz = self.exec_command("ls -l /var/lib/mysql/ib_logfile1 | awk '{print $5}'", ignore_failure = True, capture_output=True)[0].strip()
+        if siz == self.mysql_redo_log_sz:
+            self.exec_command("rm -rf /var/lib/mysql/ib_logfile*")
 
 def main(args_str = None):
     galera = GaleraSetup(args_str)
