d9e065d12640acb912750007792fa0c1f3d5c3a4
root <dgautam@juniper.net>
2015-08-13 10:11:46 -0700

    SM-TOR: Configure host_control_ip for tor node.
    CLoses-Bug: #1480873
    
    Testing:
    1. provisioned cluster with TOR config and verified that control_network_ip is
    populated with host control ip address.
    
    Conflicts:
    
    	contrail/environment/modules/contrail/manifests/toragent.pp
    
    Change-Id: I46488f6e2c06c576d8e22c555b6711c798b6fdde

--- contrail/environment/modules/contrail/manifests/toragent.pp
+++ contrail/environment/modules/contrail/manifests/toragent.pp
@@ -10,6 +10,7 @@ class contrail::toragent(
     $keystone_admin_password = $::contrail::params::keystone_admin_password,
     $keystone_admin_tenant = $::contrail::params::keystone_admin_tenant,
     $haproxy = $::contrail::params::haproxy,
+    $host_control_ip = $::contrail::params::host_ip
 ) {
     $config_ip_to_use = $::contrail::params::config_ip_to_use
     $discovery_ip_to_use = $::contrail::params::discovery_ip_to_use
@@ -26,6 +27,7 @@ class contrail::toragent(
         'host_control_ip'         => $host_control_ip,
         'product_name'            => "",
         'keepalive_time'          => '10000'
+        'host_control_ip'         => $host_control_ip
     }
     contrail::lib::report_status { 'toragent_started': state => 'toragent_started' }
     $tor_config = hiera('contrail::params::top_of_rack', {})
