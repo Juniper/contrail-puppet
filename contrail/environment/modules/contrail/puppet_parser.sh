f5af732dfd6e9504333d687b3d17719fa599d456
root <dgautam@juniper.net>
2015-08-29 02:26:23 -0700

    SM-QUENCH: added puppet-lint to puppet-parser.sh
    Change-Id: I6e2327c71a21677c4ad835cba3524651283c94d9
    Partial-bug: #1472554

--- contrail/environment/modules/contrail/puppet_parser.sh
+++ contrail/environment/modules/contrail/puppet_parser.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 for file in $(find ./manifests -iname '*.pp')
 do
-      echo " Parsing ${file} "
+      echo " Validating ${file} "
         puppet parser validate \
         --render-as s \
         --modulepath=modules \
         "${file}" || exit 1;
 done
 
+for file in $(find ./manifests -iname '*.pp')
+do
+      echo " Checking with puppet-lint ${file} "
+        puppet-lint --no-80chars-check \
+        "${file}" || exit 1;
+done
