2f41656e1579a4951e92734abe76621fbdf0fa89
tsurendra <tsurendra@juniper.net>
2015-10-06 11:35:13 -0700

    Partial-Bug: #1498195     Server Manager: Make the apache vhost on smgr which host packages to     listen on 80
        1)Http request for the repo are made with hostname as puppet instead
    of contrail_repo_ip_addr
    
    Conflicts:
    
    	contrail/environment/modules/contrail/manifests/lib/contrail_setup_repo.pp
    
    Change-Id: Ic03b1dca06e92927688324a4e69d726511c22242

--- contrail/environment/modules/contrail/manifests/lib/contrail_setup_repo.pp
+++ contrail/environment/modules/contrail/manifests/lib/contrail_setup_repo.pp
@@ -12,7 +12,7 @@ define contrail::lib::contrail_setup_repo(
     }
     if ($operatingsystem == "Ubuntu") {
         apt::source { "contrail_${contrail_repo_name}":
-          location => "http://$contrail_repo_ip:9003/contrail/repo/${contrail_repo_name}",
+          location => "http://$contrail_repo_ip/contrail/repo/${contrail_repo_name}",
           repos    => 'main',
           release  => 'contrail',
        }
