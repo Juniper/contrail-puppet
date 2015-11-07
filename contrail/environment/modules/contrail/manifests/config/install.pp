94dbfb9dbd14f27cbcf275503b3239430e147677
tsurendra <tsurendra@juniper.net>
2015-11-07 14:44:54 -0800

    Closes-Bug: #1513850
    Fetch Correct keepalived package for ubuntu12.04.
    
    Testing:
    Verified on ubuntu 12-04
    that the correct keepalived was getting installed.
    
    Change-Id: I03c1ea33e83b23f6b5503670f9c16ac03415d93c

--- contrail/environment/modules/contrail/manifests/config/install.pp
+++ contrail/environment/modules/contrail/manifests/config/install.pp
@@ -1,4 +1,28 @@
 class contrail::config::install() {
+  # Install a specific version of keepalived in non-ha
+  # case also, to support upgrade of contrail software.
+  # Below code is not required when keepalive dependency is fixed.
+  #
+  if ($contrail_internal_vip == "" and ($internal_vip == "" or !('openstack' in $contrail_host_roles))) {
+
+      if ($lsbdistrelease == "14.04") {
+          $keepalived_pkg         = '1.2.13-0~276~ubuntu14.04.1'
+      } else {
+          $keepalived_pkg         = '1:1.2.13-1~bpo70+1'
+      }
+
+      package { 'keepalived' :
+          ensure => $keepalived_pkg,
+          before => Package['contrail-openstack-config'],
+      }
+      ->
+      service { "keepalived" :
+          enable => false,
+          require => [ Package['keepalived']],
+          ensure => stopped,
+          before => Package['contrail-openstack-config'],
+      }
+  }
 
   package { 'contrail-openstack-config' :
     ensure => latest
