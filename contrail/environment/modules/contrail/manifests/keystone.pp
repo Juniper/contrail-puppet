4e139539d1499c65c10fa2ed998f23647d31a8be
root <dgautam@juniper.net>
2015-11-26 03:28:35 -0800

    SM-QUENCH: controller refactor and other changes
    Partial-Bug: #1472554
    
    1. Commented out old and unsed init.d code
    2. remove unused puppet files.
    3. removed unwanted template files.
    4. put two service resource into a single resource with an array.
    5. copied type/providers from puppet-contrail from redhat
    6. added new type/provider for collector-snmp.ini
    7. included keystone instead of using if !defined (resource)
    8. moved templates to config resources
    
    TESTING:
    =========
    1. provisioned 1 all-in-one node, verified that provision is completed
    2. added image, created a network, subnetwork and 2 VMs, verified VMs are
    active.
    3. Logged-in to contrail-web ui and browsed to verify web-ui is up and
    Running.
    4. VMs are able to ping each other.
    5. VM is able get http://169.254.269.254/openstack (metadata service)
    6. Restarted the system, system cameup, verified contrail-status is proper and
    all services are active.
    7. Started previously launched VMs. Launched one more VM and verified ping
    works,
    metadata service is working for newer VM.
    8. Verified one of the previously launched VM for ping and metadata service
    
    Change-Id: I81d7954cb45752f0378d7266caa5077a6f5503f5

---
+++ contrail/environment/modules/contrail/manifests/keystone.pp
@@ -0,0 +1,25 @@
+class contrail::keystone (
+    $keystone_ip_to_use = $::contrail::params::keystone_ip_to_use,
+    $keystone_auth_protocol = $::contrail::params::keystone_auth_protocol,
+    $keystone_auth_port = $::contrail::params::keystone_auth_port,
+    $keystone_admin_user = $::contrail::params::keystone_admin_user,
+    $keystone_admin_password = $::contrail::params::keystone_admin_password,
+    $keystone_admin_tenant = $::contrail::params::keystone_admin_tenant,
+    $keystone_insecure_flag = $::contrail::params::keystone_insecure_flag,
+    $multi_tenancy = $::contrail::params::multi_tenancy,
+) {
+  contrail_keystone_auth_config {
+    'KEYSTONE/auth_host' : value => $keystone_ip_to_use;
+    'KEYSTONE/auth_protocol' : value => $keystone_auth_protocol;
+    'KEYSTONE/auth_port' : value => $keystone_auth_port;
+    'KEYSTONE/admin_user' : value => $keystone_admin_user;
+    'KEYSTONE/admin_password' : value => $keystone_admin_password;
+    'KEYSTONE/admin_tenant_name' : value => $keystone_admin_tenant;
+    'KEYSTONE/insecure' :  value => $keystone_insecure_flag;
+  }
+  if $multi_tenancy == true {
+    contrail_keystone_auth_config {
+      'KEYSTONE/memcache_servers' : value => '127.0.0.1:11211'
+    }
+  }
+}
