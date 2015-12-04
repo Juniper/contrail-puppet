a787ea71508752dc70305acc45662580772c83ea
root <dgautam@juniper.net>
2015-12-04 00:38:46 -0800

    SM-QUENCH: removed old unused code/files
    Partial-Bug: #1472554
    
    TESTING:
    =========
    1. provisioned 1 all-in-one node, verified that provision is completed
    2. added image, created a network, subnetwork and 2 VMs, verified VMs are
    active.
    3. Logged-in to contrail-web ui and browsed to verify web-ui is up and
    Running.
    4. VMs are able to ping each other.
    5. VM is able get http://169.254.269.254/openstack (metadata service)
    
    Change-Id: I997ebf61e35ef78c10aa257840584ede30580cc8

--- contrail/environment/modules/contrail/manifests/haproxy/config.pp
+++ contrail/environment/modules/contrail/manifests/haproxy/config.pp
@@ -11,37 +11,18 @@ class contrail::haproxy::config(
     $contrail_logoutput = $::contrail::params::contrail_logoutput,
     $tor_ha_proxy_config = $::contrail::params::tor_ha_config
 ) {
-    notify {"Haproxy - tor_ha_config => ${tor_ha_proxy_config}":;}
-    # Debug - Print all variables
-    notify { "Haproxy - config_ip_list = ${config_ip_list}":; }
-    notify { "Haproxy - config_name_list = ${config_name_list}":;}
-    notify { "Haproxy - openstack_ip_list = ${openstack_ip_list}":; }
-    notify { "Haproxy - openstack_name_list = ${openstack_name_list}":;}
-    notify { "Haproxy - collector_ip_list = ${collector_ip_list}":; }
-    notify { "Haproxy - collector_name_list = ${collector_name_list}":;}
-    notify { "Haproxy - internal_vip = ${internal_vip}":; }
-    notify { "Haproxy - contrail_internal_vip = ${contrail_internal_vip}":;}
-    notify { "Haproxy - host_ip = ${host_ip}":;}
-    $manage_amqp = 'no'
 
-    if ($internal_vip == undef or $internal_vip == '') {
-        $ha_internal_vip = 'none'
-    } else {
-        $ha_internal_vip = $internal_vip
-    }
-    if ($contrail_internal_vip == undef or $contrail_internal_vip == '') {
-        $ha_contrail_internal_vip = 'none'
-    } else {
-        $ha_contrail_internal_vip = $contrail_internal_vip
-    }
+    $ha_internal_vip = pick($internal_vip, 'none')
+    $ha_contrail_internal_vip = pick($contrail_internal_vip, 'none')
 
-    $openstack_ip_list_shell = inline_template('<%= @openstack_ip_list.map{ |name2| "#{name2}" }.join(",") %>')
-    $config_ip_list_shell = inline_template('<%= @config_ip_list.map{ |name2| "#{name2}" }.join(",") %>')
-    $collector_ip_list_shell = inline_template('<%= @collector_ip_list.map{ |name2| "#{name2}" }.join(",") %>')
+    $openstack_ip_list_shell   = join($openstack_ip_list, ",")
+    $openstack_name_list_shell = join($openstack_name_list, ",")
 
-    $openstack_name_list_shell = inline_template('<%= @openstack_name_list.map{ |name2| "#{name2}" }.join(",") %>')
-    $config_name_list_shell = inline_template('<%= @config_name_list.map{ |name2| "#{name2}" }.join(",") %>')
-    $collector_name_list_shell = inline_template('<%= @collector_name_list.map{ |name2| "#{name2}" }.join(",") %>')
+    $config_ip_list_shell   = join($config_ip_list, ",")
+    $config_name_list_shell = join($config_name_list, ",")
+
+    $collector_ip_list_shell   = join($collector_ip_list, ",")
+    $collector_name_list_shell = join($collector_name_list, ",")
 
     $contrail_exec_haproxy_gen = "python /opt/contrail/bin/generate_haproxy.py  && echo generate_ha_config >> /etc/contrail/contrail_openstack_exec.out"
 
--- contrail/environment/modules/openstack/manifests/profile/contrail/webui.pp
+++
@@ -1,4 +0,0 @@
-# The puppet module to set up a Contrail WebUI server
-class openstack::profile::contrail::webui {
-    include ::contrail::webui
-}
