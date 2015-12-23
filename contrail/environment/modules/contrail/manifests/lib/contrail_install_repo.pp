12660afc3f316218404342920ea693ada0eba349
Abhay Joshi <abhayj@juniper.net>
2015-12-22 17:38:09 -0800

    Closes-Bug: #1528723     Remove installation of "contrail-install-packages" on target nodes. It is not needed and is causing     problems when installing and provisioning using contrail.tgz instead of contrail.deb.
    Change-Id: I451b77ed5af18965c1476a416215be79b604e639

--- contrail/environment/modules/contrail/manifests/lib/contrail_install_repo.pp
+++ contrail/environment/modules/contrail/manifests/lib/contrail_install_repo.pp
@@ -2,39 +2,26 @@
 define contrail::lib::contrail_install_repo(
   $contrail_logoutput = false,
 ) {
-  $contrail_repo_type = $name
+    package { ['contrail-fabric-utils', 'contrail-setup' ] :
+        ensure => latest
+    } ->
 
-    if (($contrail_repo_type == "contrail-ubuntu-package") or
-        ($contrail_repo_type == "contrail-centos-repo")) {
-        $package_name = "contrail-install-packages"
+    exec { "exec-pip-install-fabric" :
+        command => "pip install /opt/contrail/python_packages/Fabric-1.7.5.tar.gz && echo exec-pip-install-fabric >> /etc/contrail/contrail_common_exec.out",
+        provider => shell,
+        unless => "grep -qx exec-pip-install-fabric /etc/contrail/contrail_common_exec.out",
+        logoutput => $contrail_logoutput
     }
-    else {
-        $package_name = ''
-    }
+    #Untill we upgrade to latest puppet , commenting this out
+    #package {Fabric: ensure => present, provider => pip, install_options => ['--find-links=file://opt/contrail/python_packages']}
+    # May need to install fabric-utils here. below commented out code is kept for reference, in case needed.
+    # pip install --upgrade --no-deps --index-url='' /opt/contrail/python_packages/Fabric-*.tar.gz
 
-    if ( $package_name != '' ) {
-        package {$package_name: ensure => latest, install_options => '--force-yes'} ->
-        package { ['contrail-fabric-utils', 'contrail-setup' ] :
-            ensure => latest
-        } ->
-
-        exec { "exec-pip-install-fabric" :
-            command => "pip install /opt/contrail/python_packages/Fabric-1.7.5.tar.gz && echo exec-pip-install-fabric >> /etc/contrail/contrail_common_exec.out",
-            provider => shell,
-            unless => "grep -qx exec-pip-install-fabric /etc/contrail/contrail_common_exec.out",
-            logoutput => $contrail_logoutput
-        }
-        #Untill we upgrade to latest puppet , commenting this out
-        #package {Fabric: ensure => present, provider => pip, install_options => ['--find-links=file://opt/contrail/python_packages']}
-        # May need to install fabric-utils here. below commented out code is kept for reference, in case needed.
-        # pip install --upgrade --no-deps --index-url='' /opt/contrail/python_packages/Fabric-*.tar.gz
-
-        # disabled sun-java-jre and sun-java-bin prompt during installation, add oracle license acceptance in debconf
-        # disable prompts during java installation and oracle license acceptance
-        exec { "exec-disable-jre-prompts" :
-    	    command => "echo 'sun-java6-plugin shared/accepted-sun-dlj-v1-1 boolean true' | /usr/bin/debconf-set-selections; echo 'sun-java6-bin shared/accepted-sun-dlj-v1-1 boolean true' | /usr/bin/debconf-set-selections; echo 'sun-java6-jre shared/accepted-sun-dlj-v1-1 boolean true' | /usr/bin/debconf-set-selections; echo 'debconf shared/accepted-oracle-license-v1-1 select true' | sudo debconf-set-selections; echo 'debconf shared/accepted-oracle-license-v1-1 seen true' | sudo debconf-set-selections",
-	    provider => shell,
-	    logoutput => $contrail_logoutput
-        }
+    # disabled sun-java-jre and sun-java-bin prompt during installation, add oracle license acceptance in debconf
+    # disable prompts during java installation and oracle license acceptance
+    exec { "exec-disable-jre-prompts" :
+        command => "echo 'sun-java6-plugin shared/accepted-sun-dlj-v1-1 boolean true' | /usr/bin/debconf-set-selections; echo 'sun-java6-bin shared/accepted-sun-dlj-v1-1 boolean true' | /usr/bin/debconf-set-selections; echo 'sun-java6-jre shared/accepted-sun-dlj-v1-1 boolean true' | /usr/bin/debconf-set-selections; echo 'debconf shared/accepted-oracle-license-v1-1 select true' | sudo debconf-set-selections; echo 'debconf shared/accepted-oracle-license-v1-1 seen true' | sudo debconf-set-selections",
+        provider => shell,
+        logoutput => $contrail_logoutput
     }
 }
