d95e7aad6ef6caa496948359eb2232d41497a068
Dheeraj Gautam <dgautam@juniper.net>
2016-01-12 18:39:07 -0800

    SM-KILO: updated keepalived version for kilo/juno
    Partial-Bug: #1527720
    TESTING:
    
    Provision kilo and verified following cases:
    1. provisioned 1 all-in-one node, verified that provision is completed
    2. added image, created a network, subnetwork and 2 VMs, verified VMs are
    active.
    3. logged into VMs using 169.254.0.X ip, verfied are able to ping each other.
    4. VM is able get http://169.254.269.254/openstack (metadata service)
    
    Change-Id: I5572975a0fdd7525552076b8a3e9f0501d771c77

--- contrail/environment/modules/contrail/manifests/config/install.pp
+++ contrail/environment/modules/contrail/manifests/config/install.pp
@@ -10,29 +10,20 @@ class contrail::config::install(
   if ($contrail_internal_vip == "" and ($internal_vip == "" or !('openstack' in $contrail_host_roles))) {
 
       if ($lsbdistrelease == "14.04") {
-        case $::openstack_release {
-          'kilo': {
-            $keepalived_version = '1:1.2.7-1ubuntu1'
-          }
-          default: {
-            $keepalived_version = '1.2.13-0~276~ubuntu14.04.1'
-          }
-        }
-          $keepalived_pkg         = $keepalived_version
+          $keepalived_pkg         = '1.2.13-0~276~ubuntu14.04.1'
       } else {
           $keepalived_pkg         = '1:1.2.13-1~bpo70+1'
       }
 
       package { 'keepalived' :
           ensure => $keepalived_pkg,
-          before => Package['contrail-openstack-config'],
       }
       ->
       service { "keepalived" :
           enable => false,
           ensure => stopped,
-          before => Package['contrail-openstack-config'],
       }
+      Package['keepalived'] -> Package['contrail-openstack-config']
   }
 
   package { 'contrail-openstack-config' :
