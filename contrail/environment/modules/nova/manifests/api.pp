33316fb86f958531dff5eb361249b9a7dc6cfbb1
nitishkrishna <nitishk@juniper.net>
2016-02-23 11:29:21 -0800

    Closes-Bug: #1545618 : Upgrade of contrail from R2.23 to mainline fixed
    Changes:
    I. Cassandra upgrade thru intermediate 2.0 version
    Cassandra configured using file_line (not templates)
    Cassandra templates removed
    
    II. Python-kafka-python package replaced by python-kafka package
    
    III. Contrail-config package brings new configs, causes dependency issues - fixed
    
    IV. Removed secret key from nova api (metadata services bug)
    
    Closes-Bug: #1543333
    
    Patch 2:
    Closes-Bug: #1550650 - Error in Cassandra Seeds
    Closed-Bug: #1550665 - Error in wget of upgrade_cassandra script
    
    Patch 4:
    Changes to config_cassandra (to remove extra cofigs which cause error in multinode)
    Changes to database/install.pp (use contrail::params::contrail_repo_name - smlite CI failure)
    
    Closes-Bug: #1550650 - Previous Upgrade cassandra script has bugs
    
    1. The previous shell script did not loop properly to check for connect status
    It was succeeding in cases where cassandra comes up within five seconds only
    
    2. Config cassandra had bug in one particular file_line
    
    3. Added dependency for upgrade such that contrail-database package gets upgraded only if cassandra upgrade succeeds
    
    Note:
    upgrade_cassandra.sh can be run independently to upgrade cassandra before provision
    Usage is:
    ./upgrade_cassandra.sh <host_ip> <cassandra_home_dir> <image_id of added 3.0 image>
    
    Change-Id: I1a655b129c9878f499aef58cb1b9057c7769d53c

--- contrail/environment/modules/nova/manifests/api.pp
+++ contrail/environment/modules/nova/manifests/api.pp
@@ -202,19 +202,7 @@ class nova::api(
     'DEFAULT/metadata_workers':      value => $metadata_workers;
     'DEFAULT/use_forwarded_for':     value => $use_forwarded_for;
     'osapi_v3/enabled':              value => $osapi_v3;
-  }
-
-  if ($neutron_metadata_proxy_shared_secret){
-    nova_config {
-      'neutron/service_metadata_proxy': value => true;
-      #'DEFAULT/neutron_metadata_proxy_shared_secret':
-        #value => $neutron_metadata_proxy_shared_secret;
-    }
-  } else {
-    nova_config {
-      'neutron/service_metadata_proxy':       value  => false;
-      #'DEFAULT/neutron_metadata_proxy_shared_secret': ensure => absent;
-    }
+    'neutron/service_metadata_proxy': value => true;
   }
 
   if $auth_uri {
