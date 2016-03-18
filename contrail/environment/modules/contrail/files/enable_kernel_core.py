1151588446a0e192cd3a0257cb95302f1f42fb5a
Prasad Miriyala <pmiriyala@juniper.net>
2016-03-17 19:36:40 -0700

    Closes-Bug: #1558879, kernel crash dump is not working with server manager
    Change-Id: I1bc593ad9df469921fbfdf4afa0e4316226f1e21

--- contrail/environment/modules/contrail/files/enable_kernel_core.py
+++ contrail/environment/modules/contrail/files/enable_kernel_core.py
@@ -1,6 +1,8 @@
+import os
 import re
 import tempfile
-
+import platform
+from fabric.api import local
 
 class ExtList (list):
 
@@ -12,6 +14,15 @@ class ExtList (list):
     # end def findex
 # end class ExtList
 
+def setup_crashkernel_params(pdist, pdistversion):
+    if pdistversion == '14.04':
+        if os.path.exists('/etc/default/grub.d/kexec-tools.cfg'):
+            local(r"sed -i 's/crashkernel=.*\([ | \"]\)/crashkernel=384M-2G:64M,2G-16G:128M,16G-:256M\1/g' /etc/default/grub.d/kexec-tools.cfg")
+        if os.path.exists('/etc/default/kdump-tools'):
+            local("sed -i 's/USE_KDUMP=0/USE_KDUMP=1/' /etc/default/kdump-tools")
+    else:
+        local(r"sed -i 's/crashkernel=.*\([ | \"]\)/crashkernel=384M-2G:64M,2G-16G:128M,16G-:256M\1/g' /etc/grub.d/10_linux")
+    local("update-grub")
 
 def enable_kernel_core():
     '''
@@ -51,5 +62,9 @@ def enable_kernel_core():
 if __name__ == "__main__":
     import cgitb
     cgitb.enable(format='text')
-
-    enable_kernel_core()
+    (pdist, pdistversion, pdistrelease) = platform.dist()
+    if pdist in ['fedora', 'centos', 'redhat']:
+        enable_kernel_core()
+    if pdist == 'Ubuntu':
+        setup_crashkernel_params(pdist, pdistversion)
+        
