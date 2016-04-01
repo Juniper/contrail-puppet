7ad7683f15dc676075647ca12b7afa02f1cca67c
nitishkrishna <nitishk@juniper.net>
2016-03-31 17:55:17 -0700

    Closes-Bug: #1550650 - Re-entrant Upgrade cassandra script and upgrade fixes
    Closes-Bug: #1545618 - Duplicate upgrade bug
    This change:
    1. Fixes upgrade_cassandra.sh to be re-entrant
    2. Changes one augeas_conf_set to file line (needed as value itself has quotes)
    3. Delete of additional package: contrail-openstack-analytics needed for old python-kafka upgrade issue
    
    Change-Id: Id40eaf59629f94c26c2a6f0e2b47fa5cff7dbe81

--- contrail/environment/modules/contrail/files/upgrade_cassandra.sh
+++ contrail/environment/modules/contrail/files/upgrade_cassandra.sh
@@ -3,28 +3,37 @@ set -x
 this_host=$1; shift
 database_dir=$1; shift
 contrail_package_name=$1
+cassandra_version=''
 cassandra_version=`dpkg -s cassandra | grep Version | awk '{print $2}'`
-if [ "$cassandra_version" != "1.2.11" ]; then
+if [ "$cassandra_version" == "2.1.9" ] || [ "$cassandra_version" == '' ]; then
   # Don't do upgrade
   exit
 fi
-nodetool upgradesstables
-service supervisor-database stop
-wget -q http://puppet/contrail/repo/${contrail_package_name}/cassandra_2.0.17_all.deb
-dpkg --force-depends --force-overwrite --force-confnew --install cassandra_2.0.17_all.deb
-service cassandra stop
-chown -R cassandra:cassandra ${database_dir}
-chown -R cassandra:cassandra /var/log/cassandra
-sed -i -e "s|listen_address.*|listen_address: $this_host|" /etc/cassandra/cassandra.yaml
-sed -i -e "s|cluster_name.*|cluster_name: \'Contrail\'|" /etc/cassandra/cassandra.yaml
-sed -i -e "s|rpc_address.*|rpc_address: $this_host|" /etc/cassandra/cassandra.yaml
-sed -i -e "s|# num_tokens.*|num_tokens: 256|" /etc/cassandra/cassandra.yaml
-sed -i -e "s|initial_token.*|# initial_token:|" /etc/cassandra/cassandra.yaml
-sed -i -e "s|saved_caches_directory.*|saved_caches_directory: $database_dir/saved_caches|" /etc/cassandra/cassandra.yaml
-sed -i -e "s|commitlog_directory.*|commitlog_directory: $database_dir/commitlog|" /etc/cassandra/cassandra.yaml
-sed -i -e "s|    - /var/lib/cassandra/data|    - $database_dir/data|" /etc/cassandra/cassandra.yaml
-sed -i 's/JVM_OPTS=\"\$JVM_OPTS -Xss.*\"/JVM_OPTS=\"\$JVM_OPTS -Xss512k\"/g' /etc/cassandra/cassandra-env.sh
-service cassandra start; sleep 10
+if [ "$cassandra_version" == "1.2.11" ]; then
+  # Install intermediate, configure intermediate, and start it
+  nodetool upgradesstables
+  if [ $? != "0" ]; then
+    exit 1
+  fi
+  service supervisor-database stop
+  wget -q http://puppet/contrail/repo/${contrail_package_name}/cassandra_2.0.17_all.deb
+  dpkg --force-depends --force-overwrite --force-confnew --install cassandra_2.0.17_all.deb
+  service cassandra stop
+  chown -R cassandra:cassandra ${database_dir}
+  chown -R cassandra:cassandra /var/log/cassandra
+  sed -i -e "s|listen_address.*|listen_address: $this_host|" /etc/cassandra/cassandra.yaml
+  sed -i -e "s|cluster_name.*|cluster_name: \'Contrail\'|" /etc/cassandra/cassandra.yaml
+  sed -i -e "s|rpc_address.*|rpc_address: $this_host|" /etc/cassandra/cassandra.yaml
+  sed -i -e "s|# num_tokens.*|num_tokens: 256|" /etc/cassandra/cassandra.yaml
+  sed -i -e "s|^initial_token.*|# initial_token:|" /etc/cassandra/cassandra.yaml
+  sed -i -e "s|saved_caches_directory.*|saved_caches_directory: $database_dir/saved_caches|" /etc/cassandra/cassandra.yaml
+  sed -i -e "s|commitlog_directory.*|commitlog_directory: $database_dir/commitlog|" /etc/cassandra/cassandra.yaml
+  sed -i -e "s|    - /var/lib/cassandra/data|    - $database_dir/data|" /etc/cassandra/cassandra.yaml
+  sed -i 's/JVM_OPTS=\"\$JVM_OPTS -Xss.*\"/JVM_OPTS=\"\$JVM_OPTS -Xss512k\"/g' /etc/cassandra/cassandra-env.sh
+  service cassandra start; sleep 10
+fi
+# At this point, the 2.0.17 cassandra has been installed and has been started
+# If script is running for the second time, that means we were not able to connect to cassandra previously
 cassandra_cli_status="1"
 cassandra_cli_cmd=" "
 re_try_count=0
@@ -43,10 +52,14 @@ do
   fi
 done
 if [ $success == "1" ]; then
+  # We have successfully connected to cassandra with version 2.0.17
   echo "Successful cassandra connect to 2.0.17: $cassandra_cli_cmd"
+  nodetool upgradesstables
+  if [ $? != "0" ]; then
+    exit 1
+  fi
+  service cassandra stop
 else
   echo "Connection failure"
-  exit
+  exit 1
 fi
-nodetool upgradesstables
-service cassandra stop
