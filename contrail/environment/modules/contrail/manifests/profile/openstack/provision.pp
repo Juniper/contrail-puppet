93f7cf508a7f7012c7fcb262d81dae4db3db8dd1
Dheeraj Gautam <dgautam@juniper.net>
2016-05-19 04:13:20 -0700

    SM-HEAT-FIX: add heat provisioning as part of SM.
    Closes-Bug: #1583552
    
    Issue: Sm stopped provisioning heat, this caused all CI jobs to fail.
    
    Fix: added heat provisioning to SM/SM-lite.
    
    Test: issued "heat stack-list" as part of CI test.
    
    Change-Id: Ibdbaa38a04a4a2f2a4ab76239671dbe4a6751097

--- contrail/environment/modules/contrail/manifests/profile/openstack/provision.pp
+++ contrail/environment/modules/contrail/manifests/profile/openstack/provision.pp
@@ -4,25 +4,26 @@ class contrail::profile::openstack::provision (
   $nova_password     = $::contrail::params::os_nova_password,
   $glance_password   = $::contrail::params::os_glance_password,
   $cinder_password   = $::contrail::params::os_cinder_password,
+  $heat_password     = $::contrail::params::os_heat_password,
   $region_name       = $::contrail::params::os_region,
   $controller_mgmt_address   = $::contrail::params::os_controller_mgmt_address,
   $controller_api_address    = $::contrail::params::os_controller_api_address,
   $keystone_admin_email      = $::contrail::params::os_keystone_admin_email,
   $keystone_admin_password   = $::contrail::params::keystone_admin_password,
 ) {
-    $internal_vip = $::contrail::params::internal_vip
-    $contrail_internal_vip = $::contrail::params::contrail_internal_vip
+  $internal_vip = $::contrail::params::internal_vip
+  $contrail_internal_vip = $::contrail::params::contrail_internal_vip
 
-    if ($contrail_internal_vip != "" and $contrail_internal_vip != undef) {
-      $contrail_controller_address_api = $contrail_internal_vip
-      $contrail_controller_address_management = $contrail_internal_vip
-    } elsif ($internal_vip != "" and $internal_vip != undef) {
-      $contrail_controller_address_api = $controller_api_address
-      $contrail_controller_address_management = $controller_mgmt_address
-    } else {
-      $contrail_controller_address_api = $::contrail::params::config_ip_list[0]
-      $contrail_controller_address_management = $::contrail::params::config_ip_list[0]
-    }
+  if ($contrail_internal_vip != "" and $contrail_internal_vip != undef) {
+    $contrail_controller_address_api = $contrail_internal_vip
+    $contrail_controller_address_management = $contrail_internal_vip
+  } elsif ($internal_vip != "" and $internal_vip != undef) {
+    $contrail_controller_address_api = $controller_api_address
+    $contrail_controller_address_management = $controller_mgmt_address
+  } else {
+    $contrail_controller_address_api = $::contrail::params::config_ip_list[0]
+    $contrail_controller_address_management = $::contrail::params::config_ip_list[0]
+  }
 
   $controller_address_management = $controller_mgmt_address
   $address_api = $controller_api_address
@@ -31,47 +32,61 @@ class contrail::profile::openstack::provision (
 
   notify {"keysstone: ${keystone_admin_email} and ${keystone_admin_password}":;}
 
-    class { '::keystone::roles::admin':
-      email        => $keystone_admin_email,
-      password     => $keystone_admin_password,
-      #configure_user => false,
-      #configure_user_role => false,
-      admin_tenant => 'admin',
-      #admin_user_domain   => 'default', # domain for user
-      #admin_project_domain => 'default', # domain for project
-    } ->
-    class { 'keystone::endpoint':
-      public_url   => "http://${address_api}:5000",
-      admin_url    => "http://${controller_address_management}:35357",
-      internal_url => "http://${controller_address_management}:5000",
-      region           => $region_name,
-    } ->
-    class { '::cinder::keystone::auth':
-      password         => $cinder_password,
-      public_address   => $address_api,
-      admin_address    => $controller_address_management,
-      internal_address => $controller_address_management,
-      region           => $region_name,
-    } ->
-    class  { '::glance::keystone::auth':
-      password         => $glance_password,
-      public_address   => $storage_address_api,
-      admin_address    => $storage_address_management,
-      internal_address => $storage_address_management,
-      region           => $region_name,
-    } ->
-    class { '::nova::keystone::auth':
-      password         => $nova_password,
-      public_address   => $address_api,
-      admin_address    => $controller_address_management,
-      internal_address => $controller_address_management,
-      region           => $region_name,
-    } ->
-    class { '::neutron::keystone::auth':
-      password         => $neutron_password,
-      public_address   => $contrail_controller_address_api,
-      admin_address    => $contrail_controller_address_management,
-      internal_address => $contrail_controller_address_management,
-      region           => $region_name,
-    }
+  class { '::keystone::roles::admin':
+    email        => $keystone_admin_email,
+    password     => $keystone_admin_password,
+    #configure_user => false,
+    #configure_user_role => false,
+    admin_tenant => 'admin',
+    #admin_user_domain   => 'default', # domain for user
+    #admin_project_domain => 'default', # domain for project
+  } ->
+  class { 'keystone::endpoint':
+    public_url   => "http://${address_api}:5000",
+    admin_url    => "http://${controller_address_management}:35357",
+    internal_url => "http://${controller_address_management}:5000",
+    region           => $region_name,
+  } ->
+  class { '::cinder::keystone::auth':
+    password         => $cinder_password,
+    public_address   => $address_api,
+    admin_address    => $controller_address_management,
+    internal_address => $controller_address_management,
+    region           => $region_name,
+  } ->
+  class  { '::glance::keystone::auth':
+    password         => $glance_password,
+    public_address   => $storage_address_api,
+    admin_address    => $storage_address_management,
+    internal_address => $storage_address_management,
+    region           => $region_name,
+  } ->
+  class { '::nova::keystone::auth':
+    password         => $nova_password,
+    public_address   => $address_api,
+    admin_address    => $controller_address_management,
+    internal_address => $controller_address_management,
+    region           => $region_name,
+  } ->
+  class { '::neutron::keystone::auth':
+    password         => $neutron_password,
+    public_address   => $contrail_controller_address_api,
+    admin_address    => $contrail_controller_address_management,
+    internal_address => $contrail_controller_address_management,
+    region           => $region_name,
+  } ->
+  class { '::heat::keystone::auth':
+    password         => $heat_password,
+    public_address   => $contrail_controller_address_api,
+    admin_address    => $contrail_controller_address_management,
+    internal_address => $contrail_controller_address_management,
+    region           => $region_name,
+  }
+  class { '::heat::keystone::auth_cfn':
+    password         => $heat_password,
+    public_address   => $contrail_controller_address_api,
+    admin_address    => $contrail_controller_address_management,
+    internal_address => $contrail_controller_address_management,
+    region           => $region_name,
+  }
 }
--- contrail/environment/modules/contrail/manifests/profile/openstack_controller.pp
+++ contrail/environment/modules/contrail/manifests/profile/openstack_controller.pp
@@ -20,62 +20,60 @@ class contrail::profile::openstack_controller (
   $openstack_manage_amqp = $::contrail::params::openstack_manage_amqp,
   $neutron_ip_to_use = $::contrail::params::neutron_ip_to_use
 ) {
-    if ($enable_module and 'openstack' in $host_roles and $is_there_roles_to_delete == false) {
-        contrail::lib::report_status { 'openstack_started': state => 'openstack_started' } ->
-        package {'contrail-openstack' :
-            ensure => latest,
-            before => Class['::mysql::server']
-        } ->
-        class { 'memcached': }->
-        class {'::nova::quota' :
-              quota_instances => 10000,
-        } ->
-        class {'::contrail::profile::openstack::mysql' : } ->
-        Package['python-openstackclient'] ->
-        class {'::contrail::profile::openstack::keystone' : } ->
-        class {'::contrail::profile::openstack::glance' : } ->
-        class {'::contrail::profile::openstack::cinder' : } ->
-        service { 'supervisor-openstack': enable => true, ensure => running } ->
-        class {'::contrail::profile::openstack::nova' : } ->
-        class {'::contrail::profile::openstack::neutron' : } ->
-        #class {'::contrail::profile::openstack::heat' : } ->
-        class {'::contrail::profile::openstack::provision' : } ->
-        class {'::contrail::profile::openstack::auth_file' : } ->
-        class {'::contrail::contrail_openstack' : } ->
-        package { 'openstack-dashboard': ensure => present } ->
-        file {'/etc/openstack-dashboard/local_settings.py':
-            ensure => present,
-            mode   => '0755',
-            group  => root,
-            content => template("${module_name}/local_settings.py.erb")
-        } ->
-        contrail::lib::report_status { 'openstack_completed': state => 'openstack_completed' }
+  if ($enable_module and 'openstack' in $host_roles and $is_there_roles_to_delete == false) {
+    contrail::lib::report_status { 'openstack_started': state => 'openstack_started' } ->
+    package {'contrail-openstack' :
+      ensure => latest,
+      before => Class['::mysql::server']
+    } ->
+    class { 'memcached': }->
+    class {'::nova::quota' :
+        quota_instances => 10000,
+    } ->
+    class {'::contrail::profile::openstack::mysql' : } ->
+    Package['python-openstackclient'] ->
+    class {'::contrail::profile::openstack::keystone' : } ->
+    class {'::contrail::profile::openstack::glance' : } ->
+    class {'::contrail::profile::openstack::cinder' : } ->
+    service { 'supervisor-openstack': enable => true, ensure => running } ->
+    class {'::contrail::profile::openstack::nova' : } ->
+    class {'::contrail::profile::openstack::neutron' : } ->
+    class {'::contrail::profile::openstack::heat' : } ->
+    class {'::contrail::profile::openstack::provision' : } ->
+    class {'::contrail::profile::openstack::auth_file' : } ->
+    class {'::contrail::contrail_openstack' : } ->
+    package { 'openstack-dashboard': ensure => present } ->
+    file {'/etc/openstack-dashboard/local_settings.py':
+      ensure => present,
+      mode   => '0755',
+      group  => root,
+      content => template("${module_name}/local_settings.py.erb")
+    } ->
+    contrail::lib::report_status { 'openstack_completed': state => 'openstack_completed' }
 
-        contain ::contrail::profile::openstack::mysql
-        contain ::contrail::profile::openstack::keystone
-        contain ::contrail::profile::openstack::glance
-        contain ::contrail::profile::openstack::cinder
-        contain ::contrail::profile::openstack::nova
-        contain ::contrail::profile::openstack::neutron
-        #contain ::contrail::profile::openstack::heat
-        contain ::contrail::profile::openstack::auth_file
-        contain ::contrail::profile::openstack::provision
-        contain ::contrail::contrail_openstack
+    contain ::contrail::profile::openstack::mysql
+    contain ::contrail::profile::openstack::keystone
+    contain ::contrail::profile::openstack::glance
+    contain ::contrail::profile::openstack::cinder
+    contain ::contrail::profile::openstack::nova
+    contain ::contrail::profile::openstack::neutron
+    contain ::contrail::profile::openstack::heat
+    contain ::contrail::profile::openstack::auth_file
+    contain ::contrail::profile::openstack::provision
+    contain ::contrail::contrail_openstack
 
-        if ($enable_ceilometer) {
-            Class['::contrail::profile::openstack::heat'] ->
-            class {'::contrail::profile::openstack::ceilometer' : } ->
-            Class['::contrail::profile::openstack::auth_file']
-            contain ::contrail::profile::openstack::ceilometer
-        }
-        if ($openstack_manage_amqp and !  defined(Class['::contrail::rabbitmq']) ) {
-            contain ::contrail::rabbitmq
-            Package['contrail-openstack'] -> Class['::contrail::rabbitmq'] -> Service['supervisor-openstack']
-        }
-
-    } elsif ((!('openstack' in $host_roles)) and ($contrail_roles['openstack'] == true)) {
-
-      notify { 'uninstalling openstack':; }
-      contain ::contrail::uninstall_openstack
+    if ($enable_ceilometer) {
+      Class['::contrail::profile::openstack::heat'] ->
+      class {'::contrail::profile::openstack::ceilometer' : } ->
+      Class['::contrail::profile::openstack::auth_file']
+      contain ::contrail::profile::openstack::ceilometer
     }
+    if ($openstack_manage_amqp and !  defined(Class['::contrail::rabbitmq']) ) {
+      contain ::contrail::rabbitmq
+      Package['contrail-openstack'] -> Class['::contrail::rabbitmq'] -> Service['supervisor-openstack']
+    }
+  } elsif ((!('openstack' in $host_roles)) and ($contrail_roles['openstack'] == true)) {
+    notify { 'uninstalling openstack':; }
+    contain ::contrail::uninstall_openstack
+  }
 }
