50ddd46acf458455b71210e75398f78d78180c81
Megh Bhatt <meghb@juniper.net>
2016-05-23 16:15:05 -0700

    Increase system fd limit to 165K for database node
    Change-Id: If7560b46f662b9ef5f2deae2f3196025813664a1
    Closes-Bug: #1582100

--- contrail/environment/modules/contrail/manifests/database.pp
+++ contrail/environment/modules/contrail/manifests/database.pp
@@ -133,6 +133,10 @@ class contrail::database (
             group   => cassandra,
         }
     }
+    $database_sysctl_settings = {
+        'fs.file-max' => { value => 165535 },
+    }
+    create_resources(sysctl::value, $database_sysctl_settings, {} )
     contrail::lib::report_status { 'database_started':
         state              => 'database_started',
         contrail_logoutput => $contrail_logoutput
