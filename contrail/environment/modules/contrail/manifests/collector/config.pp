f424a9ab68e56104912baba46cef121470398387
Megh Bhatt <meghb@juniper.net>
2016-06-15 16:58:38 -0700

    Rename cloud_admin_access_only to multi_tenancy and default to False for now
    Change-Id: I4b2142c2d5e06e7c991e96ea2bf9c9e56b1082b8

--- contrail/environment/modules/contrail/manifests/collector/config.pp
+++ contrail/environment/modules/contrail/manifests/collector/config.pp
@@ -71,7 +71,7 @@ class contrail::collector::config (
       'DEFAULTS/analytics_config_audit_ttl' : value => $analytics_config_audit_ttl;
       'DEFAULTS/analytics_statistics_ttl'   : value => $analytics_statistics_ttl;
       'DEFAULTS/analytics_flow_ttl' : value => $analytics_flow_ttl;
-      'DEFAULTS/cloud_admin_access_only' : value => 'True';
+      'DEFAULTS/multi_tenancy' : value => 'False';
       'DISCOVERY/disc_server_ip'   : value => $config_ip_to_use;
       'DISCOVERY/disc_server_port' : value => '5998';
       'REDIS/redis_server_port'    : value => '6379';
