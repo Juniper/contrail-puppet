3a8d8bc4cbc7accdd1afbe1c7bc5b3054234ce01
Kamlesh Parmar <kparmar@juniper.net>
2016-07-15 20:27:54 -0700

    Partial-Bug: #1596132 Manifests changes to support server manager provisioning centos7.1 target Add workaround for rabbit not up after reboot, nova password fix, etc. Change-Id: I1096e5d5f9842637b5d5716b291960f746c95ed3

--- contrail/environment/modules/contrail/manifests/compute/service.pp
+++ contrail/environment/modules/contrail/manifests/compute/service.pp
@@ -11,7 +11,12 @@ class contrail::compute::service(
             enable  => true,
         }
     }
-    service { 'nova-compute' :
+    if ($::operatingsystem == 'Centos' or $::operatingsystem == 'Fedora') {
+        $nova_service_name = "openstack-nova-compute"
+    } else {
+        $nova_service_name = "nova-compute"
+    }
+    service { $nova_service_name :
         enable => $nova_compute_status,
         ensure => $nova_compute_status,
     }
@@ -22,5 +27,10 @@ class contrail::compute::service(
          ensure => running,
          enable => true
        }
+    }
+    Service[$nova_service_name]->
+    reboot { 'compute':
+      apply => "immediately",
+      timeout => 0,
     }
 }
