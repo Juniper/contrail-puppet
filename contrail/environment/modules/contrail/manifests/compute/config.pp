34f94b85823c56fc7ec3d9233689990c78b9e7bc
nitishkrishna <nitishk@juniper.net>
2016-07-30 16:12:07 -0700

    Closes-Bug: #1607565 - VNC not seen in Central Keystone Clusters
    The novnc proxy host should be set to host_control_ip on computes
    
    Change-Id: I16bbf1027c2e1ed931a728d2da5f1628aed20f34

--- contrail/environment/modules/contrail/manifests/compute/config.pp
+++ contrail/environment/modules/contrail/manifests/compute/config.pp
@@ -214,10 +214,7 @@ class contrail::compute::config(
       'keystone_authtoken/admin_password'=> { value => "${keystone_admin_password}" },
       'compute/compute_driver'=> { value => "libvirt.LibvirtDriver" },
       'DEFAULT/rabbit_hosts' => {value => "${nova_compute_rabbit_hosts}"},
-    }
-    if ($keystone_ip) {
-      $vnc_base_url_port = '5999'
-      nova_config { 'DEFAULT/novncproxy_base_url': value => "http://${keystone_ip}:${vnc_base_url_port}/vnc_auto.html" }
+      'DEFAULT/novncproxy_base_url' => { value => "http://${host_control_ip}:5999/vnc_auto.html" },
     }
     if (!('openstack' in $host_roles)){
       nova_config { 'glance/api_servers': value => "http://${glance_management_address}:9292"}
