b5843bf8721ff62540ccbbfe98e74a63ccfa9105
Dheeraj Gautam <dgautam@juniper.net>
2016-08-10 16:32:06 -0700

    SM-Storage: create ceph user/group and use latest package resource
    Partial Bug: #1609619
    Partial Bug: #1611446
    
    With current version of ceph, uses ceph:ceph instead of root:root. This causes
    ceph-mon to fail to come up and provisioning gets stuck.
    
    ensure latest for contrail-storage package instead of present. This will
    install latest version for package available.
    
    PATCH 2:
    add ceph:ceph for /var/lib/ceph and directory/files under /var/lib/ceph. This
    is needed for mitaka
    
    Change-Id: Id317d8d00d05deb2196f3f710d0af33ee3aeccc5

--- contrail/environment/modules/contrail/manifests/common.pp
+++ contrail/environment/modules/contrail/manifests/common.pp
@@ -40,13 +40,16 @@ class contrail::common(
     $contrail_group_details = {
       'nova'     => { gid => '499'},
       'kvm'      => { gid => '498'},
-      'libvirtd' => { gid => '497'}
+      'libvirtd' => { gid => '497'},
+      'ceph'     => { gid => '496'}
     }
     $contrail_users_details = {
       'nova'            => { ensure => present, uid => '499', gid => '499', home => '/var/lib/nova' , managehome => true},
       'libvirt-qemu'    => { ensure => present, uid => '498', gid => '498', home => '/var/lib/libvirt',  managehome => true},
       'libvirt-dnsmasq' => { ensure => present, uid => '497', gid => '497', home => '/var/lib/libvirt/dnsmasq',  managehome => true},
+      'ceph'            => { ensure => present, uid => '496', gid => '496', home => '/var/lib/ceph',  managehome => true},
     }
+
     create_resources(group, $contrail_group_details)
     create_resources(user, $contrail_users_details)
 
