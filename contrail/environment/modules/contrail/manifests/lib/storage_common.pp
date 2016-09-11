fa3dd1e9c8aa884c657f62ad64af6e3967b41a6f
Dheeraj Gautam <dgautam@juniper.net>
2016-09-11 11:22:49 -0700

    SM-Storage: configure public_network in ceph.conf
    Closes-Bug: #1595659
    
    Currently SM-Storage configures cluster_network. It should be configuring
    values of public_network instead in ceph.conf.
    
    Change-Id: I104ad1df2abb9e187d0cb3fe8e23c8de2b7a9f6d

--- contrail/environment/modules/contrail/manifests/lib/storage_common.pp
+++ contrail/environment/modules/contrail/manifests/lib/storage_common.pp
@@ -96,7 +96,7 @@ define contrail::lib::storage_common(
         fsid            => $contrail_storage_fsid,
         mon_host        => $contrail_ceph_monitors_map,
         keyring         => '/etc/ceph/$cluster.$name.keyring',
-        cluster_network => $contrail_storage_cluster_network,
+        public_network  => $contrail_storage_cluster_network,
     }
 
     $contrail_ceph_params = {
