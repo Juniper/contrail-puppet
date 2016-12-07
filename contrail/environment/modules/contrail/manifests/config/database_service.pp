36de4045b8466e4082cf14a6f5f55ee9838d5b25
nitishkrishna <nitishk@juniper.net>
2016-12-06 23:15:14 -0800

    Closes-Bug: #161526 - To fix Centos config db separation
    Config DB separation in Centos will fail in database started.
    This issue was fixed in Database role but not for Config Db provision in Centos
    
    Change-Id: Ie3a01daff00bd11cbe43467bec77d309d6966dc7

--- contrail/environment/modules/contrail/manifests/config/database_service.pp
+++ contrail/environment/modules/contrail/manifests/config/database_service.pp
@@ -1,4 +1,6 @@
-class contrail::config::database_service {
+class contrail::config::database_service (
+  $zookeeper_conf_dir = $::contrail::params::zookeeper_conf_dir,
+){
     # enable zookeeper svc so that picked by systemctl
     if ($::operatingsystem == 'Centos' or $::operatingsystem == 'Fedora') {
         exec {"chkconfig-zookeeper" :
@@ -11,7 +13,7 @@ class contrail::config::database_service {
     service { 'zookeeper':
         ensure => running,
         enable => true,
-        subscribe => File['/etc/zookeeper/conf/zoo.cfg'],
+        subscribe => File["${zookeeper_conf_dir}/zoo.cfg"],
     }
     ->
     service { 'contrail-database' :
