b540c365b216a442032b25e117cd0127d3119b09
Kamlesh Parmar <kparmar@juniper.net>
2016-12-16 01:24:01 -0800

    Closes-Bug: #1650463 Move the editing of /etc/yum/pluginconf.d/priorities.conf after yum-plugin- priorities package is installed
    Change-Id: I444bfc07c431f344a490c31bda567196a1d5062a

--- contrail/environment/modules/contrail/manifests/lib/contrail_setup_repo.pp
+++ contrail/environment/modules/contrail/manifests/lib/contrail_setup_repo.pp
@@ -8,13 +8,6 @@ define contrail::lib::contrail_setup_repo(
         file { "/etc/yum.repos.d/cobbler-config.repo" :
             ensure  => present,
             content => template("${module_name}/contrail-yum-repo.erb")
-        } ->
-        # add check_obsoletes flag off, for bug #1649596
-        exec { "/etc/yum/pluginconf.d/priorities.conf":
-            command => "echo 'check_obsoletes=1' >> /etc/yum/pluginconf.d/priorities.conf && echo exec-yum-priorities-fix >> /etc/contrail/exec-yum-pririties-fix.out",
-            provider => shell,
-            unless => "grep -qx exec-yum-priorities-fix /etc/contrail/exec-yum-pririties-fix.out",
-            logoutput => true
         }
     }
     if ($operatingsystem == "Ubuntu") {
