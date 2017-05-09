8f8da598665f46bc823e06aa85457500574a255f
Dheeraj Gautam <dgautam@juniper.net>
2017-05-08 21:11:45 -0700

    fix rabbitmq ssl puppet compilation
    Closes-Bug: #1670500
    
    1. tested u14.04.5 with amql_ssl enabled.
    
    Change-Id: Ide34ad8441cd7d5f644ae5015349e7fcdc72d88c

--- contrail/environment/modules/contrail/manifests/lib/rabbitmq_ssl.pp
+++ contrail/environment/modules/contrail/manifests/lib/rabbitmq_ssl.pp
@@ -5,17 +5,30 @@ define contrail::lib::rabbitmq_ssl(
   $kombu_ssl_keyfile  = $::contrail::params::kombu_ssl_keyfile,
 ){
   if ($rabbit_use_ssl) {
-    file {['/etc/rabbitmq','/etc/rabbitmq/ssl']:
-      ensure  => directory,
-    } ->
-    file { $kombu_ssl_certfile:
-      source => "puppet:///ssl_certs/$hostname.pem"
-    } ->
-    file { $kombu_ssl_keyfile :
-      source => "puppet:///ssl_certs/$hostname-privkey.pem"
-    } ->
-    file { $kombu_ssl_ca_certs:
-      source => "puppet:///ssl_certs/ca-cert.pem"
+    if !defined(File['/etc/rabbitmq']) {
+      file {'/etc/rabbitmq':
+        ensure  => directory,
+      }
+    }
+    if !defined(File['/etc/rabbitmq/ssl']) {
+      file {'/etc/rabbitmq/ssl':
+        ensure  => directory,
+      }
+    }
+    if !defined(File[$kombu_ssl_certfile]) {
+      file { $kombu_ssl_certfile:
+        source => "puppet:///ssl_certs/$hostname.pem"
+      }
+    }
+    if !defined(File[$kombu_ssl_keyfile]) {
+      file { $kombu_ssl_keyfile :
+        source => "puppet:///ssl_certs/$hostname-privkey.pem"
+      }
+    }
+    if !defined(File[$kombu_ssl_ca_certs]) {
+      file { $kombu_ssl_ca_certs:
+        source => "puppet:///ssl_certs/ca-cert.pem"
+      }
     }
   }
 }
