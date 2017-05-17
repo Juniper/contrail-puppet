8d138a779edd0d8f1d2a368173fbd5024f79f817
Dheeraj Gautam <dgautam@juniper.net>
2017-05-16 18:12:03 -0700

    fix sm-storage provisoining for openstack.
    Partial-Bug: #1682301
    
    1. add glance related changes
    2. added dependency on cinder-volume service
    3. add cinder.conf rbd_connection_retries to 10K
    
    Change-Id: I249b9224cbca96cd334dd6e3afde7f6627dcbc6e

--- contrail/environment/modules/contrail/manifests/lib/storage_os_config.pp
+++ contrail/environment/modules/contrail/manifests/lib/storage_os_config.pp
@@ -10,8 +10,6 @@ define contrail::lib::storage_os_config(
   }
   ceph::key {"client.$pname":
     secret          => $key,
-    cap_mon         => 'allow *',
-    cap_osd         => 'allow *',
     mode            => '0644',
     keyring_path    => "/etc/ceph/client.$pname.keyring"
   }
@@ -20,9 +18,10 @@ define contrail::lib::storage_os_config(
     cinder_config {
       "rbd-disk-$pname/rbd_pool" : value =>  $pname;
       "rbd-disk-$pname/rbd_user" : value =>  $pname;
-      "rbd-disk-$pname/volume_backend_name" : value =>  "RBD_$pname";
-      "rbd-disk-$pname/rbd_secret_uuid" : value =>  "$uuid";
-      "rbd-disk-$pname/volume_driver" : value => "cinder.volume.drivers.rbd.RBDDriver";
+      "rbd-disk-$pname/volume_backend_name"      : value => "RBD_$pname";
+      "rbd-disk-$pname/rbd_secret_uuid"          : value => "$uuid";
+      "rbd-disk-$pname/volume_driver"            : value => "cinder.volume.drivers.rbd.RBDDriver";
+      "rbd-disk-$pname/rados_connection_retries" : value => "10000";
     }
     cinder::type{$pname:
       set_key   => "volume_backend_name",
