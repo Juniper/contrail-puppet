803ffcf50a50c8cc59af4b30665f818820d90d10
Dheeraj Gautam <dgautam@juniper.net>
2017-08-31 12:45:14 -0700

    non master nodes should wait for master rabbit to come up
    Change-Id: I72f6073ae8f0026f1f87c219f95e4c14d67aa938
    Closes-Bug: #1695662
    (cherry picked from commit 31b0045795a66c64ae47105208ee0844a56add46)

--- contrail/environment/modules/contrail/manifests/verify_rabbitmq.pp
+++ contrail/environment/modules/contrail/manifests/verify_rabbitmq.pp
@@ -2,8 +2,19 @@ class contrail::verify_rabbitmq (
     $contrail_logoutput = $::contrail::params::contrail_logoutput,
     $master,
     $host_control_ip,
-    $amqp_ip_list
+    $amqp_ip_list,
+    $amqp_name_list
 ) {
+  if ($master == 'no') {
+    $master_node = $amqp_name_list[0]
+    notify{"master_node => $master_node":;}
+
+    exec {'check if first rabbitmq node is up':
+      command => "rabbitmqctl -n rabbit@${master_node}ctrl cluster_status",
+      provider  => shell,
+      logoutput => true
+    } -> File['/etc/contrail/form_rmq_cluster.sh']
+  }
   file { '/etc/contrail/form_rmq_cluster.sh' :
      mode   => '0755',
      group  => root,
